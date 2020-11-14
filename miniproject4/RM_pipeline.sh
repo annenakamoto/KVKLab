1bd83e7473605c24427e67a4fb7fc6e2565d596a
Anne Nakamoto <annen@berkeley.edu>
2020-11-13 17:19:22 -1000

    updating pipeline

--- miniproject4/RM_pipeline.sh
+++ miniproject4/RM_pipeline.sh
@@ -19,14 +19,14 @@ while read genome; do
     RepeatMasker -lib References/fngrep.fasta -dir RepeatMaskerOutput -gff -cutoff 250 -no_is -pa 24 References/$genome.fasta
     echo "ran RepeatMasker"
     
-    python KVKLab/miniproject4/RM_columns.py RepeatMaskerOutput/$genome.fasta.out | awk -v PIDENT=$PIDENT -v LENGTH=$LENGTH -v OFS='\t' '{ if (((100.0 - $4) >= PIDENT) && ($2 >= LENGTH)) { print } }' > RM_$genome_filtered.txt
+    python KVKLab/miniproject4/RM_columns.py RepeatMaskerOutput/$genome.fasta.out | awk -v PIDENT=$PIDENT -v LENGTH=$LENGTH -v OFS='\t' '{ if (((100.0 - $4) >= PIDENT) && ($2 >= LENGTH)) { print } }' > RM_filtered_$genome.txt
     echo "ran python script and filtered with awk"
 
     # use awk to convert to bed file -> filtered output bed file
-    awk -v OFS='\t' '{ print $7, $8, $9, $12 }' RM_$genome_filtered.txt > RM_$genome_filtered.bed
+    awk -v OFS='\t' '{ print $7, $8, $9, $12 }' RM_filtered_$genome.txt > RM_filtered_$genome.bed
 
     # use awk to get data -> data text file
-    awk -v genome=$genome 'BEGIN { count=0; } { count++ } END { print genome, "hits:", count; }' RM_$genome_filtered.txt > RM_$genome_data.txt
+    awk -v genome=$genome 'BEGIN { count=0; } { count++ } END { print genome, "hits:", count; }' RM_filtered_$genome.txt > RM_data_$genome.txt
 
 done < RM_pipe_in.txt
 
