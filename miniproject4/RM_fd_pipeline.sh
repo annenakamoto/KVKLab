0b4ad630bd14f610b8732c71c0545562a9571c57
Anne Nakamoto <annen@berkeley.edu>
2021-01-04 17:47:01 -1000

    using fngrep and guy11 denovo library with no unknowns

--- miniproject4/RM_fd_pipeline.sh
+++ miniproject4/RM_fd_pipeline.sh
@@ -16,7 +16,7 @@ LENGTH=$2
 while read genome; do
 
     # run RepeatMasker
-    # RepeatMasker -lib References/fngrep_guy11denovo.fasta -dir RepeatMaskerOutput -gff -cutoff 250 -no_is -pa 24 References/$genome.fasta
+    RepeatMasker -lib References/fng_guy11dn_noUN.fasta -dir RepeatMaskerOutput -gff -cutoff 250 -no_is -pa 24 References/$genome.fasta
     
     # run python script on RM output, pipe to awk for filtering
     python KVKLab/miniproject4/RM_fd_columns.py RepeatMaskerOutput/$genome.fasta.out | awk -v PIDENT=$PIDENT -v LENGTH=$LENGTH -v OFS='\t' '{ if (((100.0 - $4) >= PIDENT) && ($2 >= LENGTH)) { print } }' > RM_fd_filtered_$genome.txt
