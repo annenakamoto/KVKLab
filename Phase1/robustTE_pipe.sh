0da1f04f7bc974c784e6615aa852cf32c3896620
Anne Nakamoto <annen@berkeley.edu>
2021-03-25 03:34:59 -1000

    comprehensice lib

--- Phase1/robustTE_pipe.sh
+++ Phase1/robustTE_pipe.sh
@@ -11,32 +11,37 @@
 cd /global/scratch/users/annen/
 source activate /global/scratch/users/annen/anaconda3/envs/RepeatModeler
 
-GENOME=$1
-
-# run RepeatModeler on GENOME
-#BuildDatabase -name rmdb_$GENOME -engine ncbi hq_genomes/$GENOME.fasta
-#RepeatModeler -engine ncbi -pa 24 -database rmdb_$GENOME -LTRStruct -ninja_dir /global/scratch/users/annen/NINJA-0.95-cluster_only/NINJA
-
-# run IRF on GENOME
-#./irf307.exe hq_genomes/$GENOME.fasta 2 3 5 80 10 20 500000 10000 -a3 -t4 1000 -t5 5000 -h -d -ngs > irf_$GENOME.dat
-
-# parse irf output into fasta format
-#awk 'BEGIN { count=0; } { if ($18 != "") { count++; print ">irf-" count "_left#DNA/IRF" "\n" $18 "\n" ">irf-" count "_right#DNA/IRF" "\n" $19 } }' irf_$GENOME.dat > irf_$GENOME.fasta
+while read GENOME; do
+    sbatch -W KVKLab/Phase1/robustTE_denovo.sh $GENOME
+done < KVKLab/Phase1/robustTE_pipe_in.txt
+wait
 
 # combine RepeatModleler, IRF, and RepBase (References/fngrep.fasta) libraries
-# run CD-HIT to remove repeats, obtain high quality TE library for GENOME
-#cat References/fngrep.fasta rmdb_$GENOME-families.fa irf_$GENOME.fasta > unclib_$GENOME.fasta
-#cd-hit-est -i unclib_$GENOME.fasta -o clustlib_$GENOME.fasta -c 1.0 -aS 0.99 -g 1 -d 0 -T 24
+cat References/fngrep.fasta > unclib.fasta
+while read GENOME; do
+    cat unclib.fasta rmdb_$GENOME-families.fa > unclib_$GENOME.fasta
+done < KVKLab/Phase1/robustTE_pipe_in.txt
+
+while read GENOME; do
+    cat unclib.fasta irf_$GENOME.fasta > unclib_$GENOME.fasta
+done < KVKLab/Phase1/robustTE_pipe_in.txt
+
+# run CD-HIT to remove repeats, obtain high quality comprehensive TE library
+cd-hit-est -i unclib.fasta -o clustlib.fasta -c 1.0 -aS 0.99 -g 1 -d 0 -T 24
 
 # parse the clustered library to prioritize RepBase, RepeatModeler, then IRF to be the representative element
-#awk 'BEGIN { max=0; clust=0; }
-#    />Cluster/ { max=0; clust=$2 }
-#    !/>irf-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-#    />irf-/ && /\*/ { if(max==0) {  max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-#    END { for(i in a) { print a[i]; } }' clustlib_$GENOME.fasta.clstr | python KVKLab/Phase1/robustTE_prioritize.py unclib_$GENOME.fasta > LIB_$GENOME.fasta
+awk 'BEGIN { max=0; clust=0; }
+    />Cluster/ { max=0; clust=$2 }
+    !/>irf-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+    />irf-/ && /\*/ { if(max==0) {  max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+    END { for(i in a) { print a[i]; } }' clustlib.fasta.clstr | python KVKLab/Phase1/robustTE_prioritize.py unclib.fasta > LIB.fasta
 
-# run RepeatMasker on GENOME using high quality TE library
-RepeatMasker -lib LIB_$GENOME.fasta -dir robustTE_RepeatMaskerOut -gff -cutoff 200 -no_is -nolow -pa 24 -gccalc hq_genomes/$GENOME.fasta
+
+# scan library for HMM PFAM profile domains using pfam_scan.pl
+# scan library for CDD profile domains using RPS-BLAST
+
+# run RepeatMasker on GENOME using high quality TE library that was scanned for domains
+#RepeatMasker -lib <domain-scanned lib> -dir robustTE_RepeatMaskerOut -gff -cutoff 200 -no_is -nolow -pa 24 -gccalc hq_genomes/$GENOME.fasta
 
 # scan output for HMM PFAM profile domains using pfam_scan.pl
 # scan output for CDD profile domains using RPS-BLAST
