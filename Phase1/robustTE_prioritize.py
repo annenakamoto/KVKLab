03a51486d36a4f011e3c625dc57b18a95f7bfa39
Anne Nakamoto <annen@berkeley.edu>
2021-03-25 01:51:20 -1000

    no newline issues

--- Phase1/robustTE_prioritize.py
+++ Phase1/robustTE_prioritize.py
@@ -8,12 +8,13 @@ with open(sys.argv[1], "r") as library:
     printing = False
     for line in library:
         words = str(line).split()
-        first_word = words[0]
-        if first_word[0] == ">":
-            if first_word in elements:
-                printing = True
-            else:
-                printing = False
-        if printing:
-            print(line[:-1])
+        if len(words) > 0:
+            first_word = words[0]
+            if first_word[0] == ">":
+                if first_word in elements:
+                    printing = True
+                else:
+                    printing = False
+            if printing:
+                print(line[:-1])
         
