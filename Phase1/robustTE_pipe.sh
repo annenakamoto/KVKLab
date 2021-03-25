cc45e57671576b76531b76d5ae7c7e43a2fac2c8
Anne Nakamoto <annen@berkeley.edu>
2021-03-25 02:31:13 -1000

    repatmasker

--- Phase1/robustTE_pipe.sh
+++ Phase1/robustTE_pipe.sh
@@ -25,18 +25,18 @@ GENOME=$1
 
 # combine RepeatModleler, IRF, and RepBase (References/fngrep.fasta) libraries
 # run CD-HIT to remove repeats, obtain high quality TE library for GENOME
-cat References/fngrep.fasta rmdb_$GENOME-families.fa irf_$GENOME.fasta > unclib_$GENOME.fasta
+#cat References/fngrep.fasta rmdb_$GENOME-families.fa irf_$GENOME.fasta > unclib_$GENOME.fasta
 #cd-hit-est -i unclib_$GENOME.fasta -o clustlib_$GENOME.fasta -c 1.0 -aS 0.99 -g 1 -d 0 -T 24
 
 # parse the clustered library to prioritize RepBase, RepeatModeler, then IRF to be the representative element
-awk 'BEGIN { max=0; clust=0; }
-    />Cluster/ { max=0; clust=$2 }
-    !/>irf-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-    />irf-/ && /\*/ { if(max==0) {  max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-    END { for(i in a) { print a[i]; } }' clustlib_$GENOME.fasta.clstr | python KVKLab/Phase1/robustTE_prioritize.py unclib_$GENOME.fasta > LIB_$GENOME.fasta
+#awk 'BEGIN { max=0; clust=0; }
+#    />Cluster/ { max=0; clust=$2 }
+#    !/>irf-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+#    />irf-/ && /\*/ { if(max==0) {  max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+#    END { for(i in a) { print a[i]; } }' clustlib_$GENOME.fasta.clstr | python KVKLab/Phase1/robustTE_prioritize.py unclib_$GENOME.fasta > LIB_$GENOME.fasta
 
 # run RepeatMasker on GENOME using high quality TE library
-#RepeatMasker -lib clustlib_$GENOME.fasta -dir robustTE_RepeatMaskerOut -gff -cutoff 200 -no_is -nolow -pa 24 -gccalc hq_genomes/$GENOME.fasta
+RepeatMasker -lib LIB_$GENOME.fasta -dir robustTE_RepeatMaskerOut -gff -cutoff 200 -no_is -nolow -pa 24 -gccalc hq_genomes/$GENOME.fasta
 
 # scan output for HMM PFAM profile domains using pfam_scan.pl
 # scan output for CDD profile domains using RPS-BLAST
