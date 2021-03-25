3a9249202eddf72398c53e8caac3ad13280c33ab
Anne Nakamoto <annen@berkeley.edu>
2021-03-24 22:17:14 -1000

    partition

--- Phase1/robustTE_prioritize.py
+++ Phase1/robustTE_prioritize.py
@@ -7,7 +7,7 @@ for elem in sys.stdin:
 with open(sys.argv[1], "r") as library:
     printing = False
     for line in library:
-        first_word = line.split()[0]
+        first_word = line.partition(' ')[0]
         if first_word[0] == ">":
             if first_word in elements:
                 elements.remove(first_word)
