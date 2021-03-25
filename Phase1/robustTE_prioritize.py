922f83ee989700a1775021ebbd934f6703d6bb55
Anne Nakamoto <annen@berkeley.edu>
2021-03-25 00:47:33 -1000

    no newline

--- Phase1/robustTE_prioritize.py
+++ Phase1/robustTE_prioritize.py
@@ -2,20 +2,19 @@ import sys
 
 elements = []
 for elem in sys.stdin:
-    elements.append(elem)
+    elements.append(elem[:-1])
 
 with open(sys.argv[1], "r") as library:
     printing = False
     for line in library:
-        words = line.split()
-        if len(words) > 0:
-            first_word = words[0]
-            if first_word[0] == ">":
-                if first_word in elements:
-                    elements.remove(first_word)
-                    printing = True
-                else:
-                    printing = False
+        words = line.partition(' ')
+        first_word = words[0]
+        if first_word[0] == ">":
+            if first_word in elements:
+                elements.remove(first_word)
+                printing = True
+            else:
+                printing = False
         if printing:
             print(line)
         
