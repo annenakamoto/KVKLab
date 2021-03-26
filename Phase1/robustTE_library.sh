7e1a5c39f7ac8c216265763e4aa6314295960c5b
Anne Nakamoto <annen@berkeley.edu>
2021-03-25 23:44:34 -1000

    adding genome names to elements

--- Phase1/robustTE_library.sh
+++ Phase1/robustTE_library.sh
@@ -14,7 +14,7 @@ source activate /global/scratch/users/annen/anaconda3/envs/RepeatModeler
 # combine RepeatModleler, IRF, and RepBase (References/fngrep.fasta) libraries
 cat References/fngrep.fasta > unclib.fasta
 while read GENOME; do
-    cat unclib.fasta rmdb_$GENOME-families.fa > unclib.fasta
+    cat unclib.fasta rmdb_$GENOME-families.fasta > unclib.fasta
 done < KVKLab/Phase1/robustTE_pipe_in.txt
 
 while read GENOME; do
@@ -25,11 +25,10 @@ done < KVKLab/Phase1/robustTE_pipe_in.txt
 cd-hit-est -i unclib.fasta -o clustlib.fasta -c 1.0 -aS 0.99 -g 1 -d 0 -T 24
 
 # parse the clustered library to prioritize RepBase, RepeatModeler, then IRF to be the representative element
-awk 'BEGIN { max=0; clust=0; }
-    />Cluster/ { max=0; clust=$2 }
-    !/>irf-|>ltr-|>rnd-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-    />ltr-/ && /\*/ { if(max==0) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-    />rnd-/ && /\*/ { if(max==0) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+awk 'BEGIN { max=0; clust=0; rb=0; }
+    />Cluster/ { max=0; rb=0; clust=$2 }
+    !/>irf-|>ltr-|>rnd-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3); rb=1 } }
+    />ltr-/ || />rnd-/ { if(max==0 || rb==0 && substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
     />irf-/ && /\*/ { if(max==0) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
     END { for(i in a) { print a[i]; } }' clustlib.fasta.clstr > LIB_list.txt # | python KVKLab/Phase1/robustTE_prioritize.py unclib.fasta > LIB.fasta
 
