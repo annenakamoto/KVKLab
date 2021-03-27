146ac812a177484b0c4360112f588d06404a31ac
Anne Nakamoto <annen@berkeley.edu>
2021-03-27 02:24:07 -1000

    range

--- Phase1/robustTE_prioritize.py
+++ Phase1/robustTE_prioritize.py
@@ -12,7 +12,7 @@ with open(sys.argv[1], "r") as library:
             first_word = words[0]
             if first_word[0] == ">":
                 printing = False
-                for i in elements:
+                for i in range(len(elements)):
                     if elements[i] == first_word:
                         printing = True
                         elements.pop(i)
