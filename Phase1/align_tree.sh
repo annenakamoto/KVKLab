ceb8465737cc910018ed334978f030a17ae23912
Anne Nakamoto <annen@berkeley.edu>
2021-05-18 00:30:02 -1000

    deepTE

--- Phase1/align_tree.sh
+++ Phase1/align_tree.sh
@@ -10,8 +10,15 @@
 #SBATCH --mail-type=ALL
 cd /global/scratch/users/annen/
 
+source activate /global/scratch/users/annen/anaconda3/envs/DeepTE
+
+./DeepTE.py -i LIB_DOM.fasta -sp F -m F
+
+
+source deactivate
+
 # produce alignment of all the elements in LIB_DOM.fasta
-muscle -in LIB_DOM.fasta -maxiters 2 > LIB_DOM_align.fasta
+#muscle -in LIB_DOM.fasta -maxiters 2 > LIB_DOM_align.fasta
 
 # generate tree using RAXML
-raxml -s LIB_DOM_align.fasta -n raxml -m GTRCAT -f a -x 1123 -p 2341 -# 100
+#raxml -s LIB_DOM_align.fasta -n raxml -m GTRCAT -f a -x 1123 -p 2341 -# 100
