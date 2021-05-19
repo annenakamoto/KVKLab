1988c464a2571a15fbe73340649b70b57dba15d7
Anne Nakamoto <annen@berkeley.edu>
2021-05-19 11:51:39 -1000

    in dir

--- Phase1/align_tree.sh
+++ Phase1/align_tree.sh
@@ -9,16 +9,16 @@
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=ALL
 
-#cd /global/scratch/users/annen
-#source activate /global/scratch/users/annen/anaconda3/envs/DeepTE
-
-#anaconda3/envs/DeepTE/DeepTE.py -i References/fngrep.fasta -sp F -m F #-prop_thr 0.9
-
-#source deactivate
-
 cd /global/scratch/users/annen
+source activate /global/scratch/users/annen/anaconda3/envs/DeepTE
 
-MeShClust/MeShClust-1.0.0/bin/meshclust References/fngrep.fasta --id 0.80 --output fngrep.clstr
+./DeepTE.py -i fngrep.fasta -sp F -m F #-prop_thr 0.9
+
+source deactivate
+
+#cd /global/scratch/users/annen
+
+#MeShClust/MeShClust-1.0.0/bin/meshclust References/fngrep.fasta --id 0.80 --output fngrep.clstr
 
 # produce alignment of all the elements in LIB_DOM.fasta
 #muscle -in LIB_DOM.fasta -maxiters 2 > LIB_DOM_align.fasta
