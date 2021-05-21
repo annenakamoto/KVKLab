3374b2e5b852859321f8f53792f13b3e1a13caa1
Anne Nakamoto <annen@berkeley.edu>
2021-05-21 02:34:39 -1000

    hq genomes

--- Phase1/busco_score.sh
+++ Phase1/busco_score.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=ALL
 cd /global/scratch/users/annen/
@@ -13,7 +13,7 @@ source activate /global/scratch/users/annen/anaconda3/envs/BUSCO
 
 while read GENOME; do
     
-    busco -i References/$GENOME.fasta -l fungi_odb10 -o busco_$GENOME -m genome -c 24 -f --out_path BUSCO_out
+    busco -i hq_genomes/$GENOME.fasta -l fungi_odb10 -o busco_$GENOME -m genome -c 24 -f --out_path BUSCO_out
 
 done < KVKLab/Phase1/robustTE_pipe_in.txt
 
