ae4f44283e11454d2be94db0c508e468fad20634
Anne Nakamoto <annen@berkeley.edu>
2021-05-26 21:33:00 -1000

    count unique classifications of RepeatClassifier

---
+++ Phase1/classifyTEs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=Robust_TE_library
+#SBATCH --account=fc_kvkallow
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+cd /global/scratch/users/annen/
+
+cat LIB_DOM.fasta | python KVKLab/Phase1/parse_classifyTEs.py > unique_classifications_RC.txt
---
+++ Phase1/parse_classifyTEs.py
@@ -0,0 +1,17 @@
+import sys
+
+ClassFam = {}
+pound = "#"
+for line in sys.stdin:
+    if pound in line:
+        classification = line.split("#")[1]
+        in_dict = ClassFam.get(classification)
+        if not in_dict:
+            ClassFam[classification] = 1
+
+count = 0
+for elem, count in ClassFam.items():
+    print(elem)
+    count += 1
+
+print(count, " total classifications.\n")
