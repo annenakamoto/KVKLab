8233111a33ee0a075a532cfa639f7e260eb17a39
Anne Nakamoto <annen@berkeley.edu>
2021-06-17 09:03:02 -0700

    parse 50

--- Phase1/classifyTEs.sh
+++ Phase1/classifyTEs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --job-name=Robust_TE_library
-#SBATCH --account=ac_kvkallow
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
@@ -11,6 +10,11 @@
 cd /global/scratch/users/annen/
 source activate /global/scratch/users/annen/anaconda3/envs/RepeatModeler
 
-cat LIB_DOM.fasta.classified | python KVKLab/Phase1/parse_classifyTEs.py > unique_classifications_RC.txt
+# find clusters for general classification at 50% identity level
+MeShClust/MeShClust-1.0.0/bin/meshclust LIB_DOM.fasta.classified --id 0.50 --delta 10 --output LIB_DOM_50.clstr --threads 24 --align
+
+# finds the classification for each cluster
+cat LIB_DOM_50.clstr | python KVKLab/Phase1/parse_classifyTEs.py > clust_class_50.txt
+
 
 source deactivate
--- Phase1/parse_classifyTEs.py
+++ Phase1/parse_classifyTEs.py
@@ -1,17 +1,21 @@
 import sys
 
-ClassFam = {}
-pound = "#"
+# make a dictionary of each cluster and its classification
+ClustClass = {}
+curClust = -1
 for line in sys.stdin:
-    if pound in line:
+    if ">Cluster" in line:
+        curClust += 1
+        ClustClass[curClust] = set()
+    if "#" in line:
         classification = (line.split("#")[1]).split()[0]
-        in_dict = ClassFam.get(classification)
-        if not in_dict:
-            ClassFam[classification] = 1
+        if classification is "Unknown":
+            fam = line.split()[3]
+            if fam is not "(":
+                ClustClass[curClust].add(fam)   # change later to add the RepeatClassifier class instead of the RepBase class
+        else:
+            ClustClass[curClust].add(classification)
+        
+for clust, class in ClustClass:
+    print(clust, "\t", ", ".join(class))
 
-count = 0
-for elem, count in ClassFam.items():
-    print(elem)
-    count += 1
-
-print(count, " total classifications.\n")
