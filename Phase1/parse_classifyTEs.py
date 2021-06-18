494499b48acd7f03eb4997f764285cfa7b57b5ed
Anne Nakamoto <annen@berkeley.edu>
2021-06-17 18:36:39 -0700

    sorted

--- Phase1/parse_classifyTEs.py
+++ Phase1/parse_classifyTEs.py
@@ -2,31 +2,35 @@ import sys
 
 # make a dictionary of each cluster and its classification
 ClustClass = {}
-curClust = 0
-clustSize = 0
-ClustClass[curClust] = set()
+curClust = -1
+
 for line in sys.stdin:
-    if ">Cluster" in line and curClust > 0:
-        if len(ClustClass[curClust]) == 0 or clustSize <= 1:
-            # throw out clusters that are unclassified or that only contain one element
-            ClustClass[curClust] = set(["Remove"])
-        curClust += 1
-        clustSize = 0
-        ClustClass[curClust] = set()
+    lst = line.split()
+    if lst[0] == ">Cluster":
+        curClust = lst[1]
+        ClustClass[lst[1]] = set()
     if "#" in line:
-        clustSize += 1
         classification = (line.split("#")[1]).split()[0]
-        if "Unknown" in classification:
-            fam = line.split()[3]
-            print(fam)
-            if fam != "(" and fam != "[":
-                ClustClass[curClust].add(fam)   # change later to add the RepeatClassifier class instead of the RepBase class, after looking at output
+        if "Unknown" in classification and lst[3]:
+            if lst[3] != "(" and lst[3] != "[" and lst[3] != "...":
+                ClustClass[curClust].add(lst[3])   # change later to add the RepeatClassifier class instead of the RepBase class, after looking at output
         else:
             ClustClass[curClust].add(classification)
-    
+
+# determine how many conflicts there are    
+lib_len = 0
+conflict = 0
 for clust, classif in ClustClass.items():
-    print(clust, "\t", ", ".join(classif))
+    lib_len += 1
+    if len(classif) == 0:
+        classif.add("REMOVE")
+    if len(classif) > 1:
+        conflict += 1
+print(conflict, "conflicts out of", lib_len, "elements in library")
+
+# print the cluster number and classifications in number order
+for i in range(0, len(ClustClass)):
+    if ClustClass.get(i):
+        print(i, "\t", ", ".join(ClustClass[i]))
 
 # write new library to LIB_CLASS.fasta (adding in classifications for each cluster and with unclassified elements removed)
-
-
