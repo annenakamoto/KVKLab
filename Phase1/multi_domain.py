e15e9b769f18898f7c548f01c7613ad4b3c5adce
Anne Nakamoto <annen@berkeley.edu>
2021-07-18 13:31:09 -0700

    right length

--- Phase1/multi_domain.py
+++ Phase1/multi_domain.py
@@ -3,13 +3,18 @@ import sys
 ALIGNMENTS = {}
 
 curr = None
+leng = 0
 for line in sys.stdin:
     if ">" in line:
         curr = line
         if not ALIGNMENTS.get(curr):
             ALIGNMENTS[curr] = str()
+        else:
+            if leng == 0:
+                leng = len(ALIGNMENTS[curr])
     else:
         ALIGNMENTS[curr] += line
 
 for key, value in ALIGNMENTS.items():
-    print(key, value)
+    if len(value) > leng:
+        print(key, value)
