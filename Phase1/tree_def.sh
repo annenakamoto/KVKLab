224dc6e796b17db06c57b4ef28d33c014e947c71
Anne Nakamoto <annen@berkeley.edu>
2021-07-25 19:58:47 -0700

    runtime

---
+++ Phase1/tree_def.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=tree_definition
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+#cd /global/scratch/users/annen/
+
