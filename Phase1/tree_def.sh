6b3bdb20345fe1be34c090fbbda4a31d87edcc67
Anne Nakamoto <annen@berkeley.edu>
2021-07-26 18:50:16 -0700

    msa with muscle

--- Phase1/tree_def.sh
+++ Phase1/tree_def.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=tree_definition
+#SBATCH --job-name=tree_msa
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
@@ -15,6 +15,7 @@ TE=$1   # RepBase element (ex. MAGGY)
 # create represenative RepBase element-specific library (ex. MAGGY library)
 # te_spec_lib.py reads from LIB_DOM_trans.fasta.classified
 cat MUSCLE_out/tree_${TE}.txt | awk ' BEGIN { FS="#" } { gsub(/ /, "_"); print $1 "#" $2 ":" $3 ; }' | python KVKLab/Phase1/te_spec_lib.py > MUSCLE_out/LIB_DOM_${TE}.fasta
+echo "created library for ${TE}"
 
 muscle -in MUSCLE_out/LIB_DOM_${TE}.fasta -out MUSCLE_out/${TE}_aligned.afa
-
+echo "completed MSA for ${TE}"
