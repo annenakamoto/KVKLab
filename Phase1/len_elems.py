d0d5cfc937df96fb3496fcd1fbb370785a5b16dc
Anne Nakamoto <annen@berkeley.edu>
2021-07-29 15:15:03 -0700

    length of elems

---
+++ Phase1/len_elems.py
@@ -0,0 +1,14 @@
+import sys
+
+leng = 0
+curr = None
+for line in sys.stdin:
+    if ">" in line:
+        if curr:
+            print(curr, "\t", leng)
+        leng = 0
+        curr = line[0]
+    else:
+        leng += len(line[:-1])
+    
+print(leng)
