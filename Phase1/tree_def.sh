6494c83f0175a2d0739256538f64f9e3dc587e4f
Anne Nakamoto <annen@berkeley.edu>
2021-07-30 00:51:55 -0700

    reo-root

--- Phase1/tree_def.sh
+++ Phase1/tree_def.sh
@@ -11,7 +11,7 @@
 cd /global/scratch/users/annen/
 
 TE=$1   # RepBase element (ex. MAGGY)
-mode=$2 # normal or representative
+mode=$2 # normal or representative or rep-root
 
 # create represenative RepBase element-specific library (ex. MAGGY library)
 # te_spec_lib.py reads from LIB_DOM.fasta.classified (nucleotide)
@@ -34,5 +34,18 @@ if [ $mode == "representative" ]; then
     mafft LIB_DOM_${TE}_rep.fasta > ${TE}_aligned_rep.afa
     echo "completed MSA for ${TE}"
     raxml -T 24 -n Raxml_${TE}_rep.out -f a -x 12345 -p 12345 -# 100 -m GTRCAT -s ${TE}_aligned_rep.afa
+    echo "ran RAXML for ${TE}"
+fi
+### REP-ROOT <ROOT>
+if [ $mode == "rep-root" ]; then
+    root=$3
+    cat MAFFT_out/tree_${TE}.txt | awk ' BEGIN { FS="#" } /MAGGY/||/GYMAG1/||/GYMAG2/||/GYPSY1/||/MGRL3/||/PYRET/||/MGR583/||/POT2/||/guy11/||/US71/||/B71/||/MZ5-1-6/||/LpKY97/||/Lh88405/ { gsub(/ /, "_"); print $1 "#" $2 ; }' > MAFFT_out/tree_${TE}_root.txt
+    echo $root >> MAFFT_out/tree_${TE}_root.txt
+    cat MAFFT_out/tree_${TE}_root.txt | python KVKLab/Phase1/te_spec_lib.py > MAFFT_out/LIB_DOM_${TE}_root.fasta
+    echo "created seq library for ${TE}"
+    cd /global/scratch/users/annen/MAFFT_out
+    mafft LIB_DOM_${TE}_root.fasta > ${TE}_aligned_root.afa
+    echo "completed MSA for ${TE}"
+    raxml -T 24 -n Raxml_${TE}_root.out -f a -x 12345 -p 12345 -# 100 -m GTRCAT -s ${TE}_aligned_root.afa
     echo "ran RAXML for ${TE}"
 fi
