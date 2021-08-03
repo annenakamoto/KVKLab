0d92128f2e06257cd24a7f4a94832f31f9f6f89a
Anne Nakamoto <annen@berkeley.edu>
2021-08-02 20:14:30 -0700

    runtime RMask

--- Phase1/robustTE_RMask.sh
+++ Phase1/robustTE_RMask.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=ALL
 cd /global/scratch/users/annen/RepeatMask_part_class
