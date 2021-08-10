944f5370fdb4303e12bf144512fe941ddb4586ac
Anne Nakamoto <annen@berkeley.edu>
2021-08-10 03:05:42 -0700

    orthofinder

---
+++ Phase3_fungap/species_tree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=species_tree
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+cd /global/scratch/users/annen/OrthoFinder_species_tree
+source activate /global/scratch/users/annen/anaconda3/envs/OrthoFinder2
+
+orthofinder -t 24 -f OrthoFinder_in
+
+conda deactivate
