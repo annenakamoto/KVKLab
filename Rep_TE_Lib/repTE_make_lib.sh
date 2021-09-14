5fae9bc9181ef91a0062417bf1e0bf55ee83eb09
Anne Nakamoto <annen@berkeley.edu>
2021-09-13 22:11:19 -0700

    cdd pfam scan

--- Rep_TE_Lib/repTE_make_lib.sh
+++ Rep_TE_Lib/repTE_make_lib.sh
@@ -34,13 +34,25 @@ cd /global/scratch/users/annen/Rep_TE_Lib
 #cd-hit-est -i REPLIB_uncl.fasta -o REPLIB_clust -c 1.0 -aS 0.99 -g 1 -d 0 -T 24 -M 0
 
 ### parse the clustered library to prioritize RepBase, RepeatModeler, then IRF to be the representative element
-awk 'BEGIN { max=0; clust=0; rb=0; }
-    />Cluster/ { max=0; rb=0; clust=$2 }
-    !/>irf-|>ltr-|>rnd-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3); rb=1 } }
-    />ltr-/ || />rnd-/ { if(max==0 || rb==0 && substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-    />irf-/ && /\*/ { if(max==0) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
-    END { for(i in a) { print a[i]; } }' REPLIB_clust.clstr > REPLIB_list.txt
+#awk 'BEGIN { max=0; clust=0; rb=0; }
+#     />Cluster/ { max=0; rb=0; clust=$2 }
+#     !/>irf-|>ltr-|>rnd-|>Cluster/ { if(substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3); rb=1 } }
+#     />ltr-/ || />rnd-/ { if(max==0 || rb==0 && substr($2, 1, length($2)-3)+0>max) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+#     />irf-/ && /\*/ { if(max==0) { max=substr($2, 1, length($2)-3)+0; a[clust]=substr($3, 1, length($3)-3) } }
+#     END { for(i in a) { print a[i]; } }' REPLIB_clust.clstr > REPLIB_list.txt
 
-cat REPLIB_list.txt | python /global/scratch/users/annen/KVKLab/Phase1/robustTE_prioritize.py REPLIB_uncl.fasta > REPLIB_clust.fasta
+#cat REPLIB_list.txt | python /global/scratch/users/annen/KVKLab/Phase1/robustTE_prioritize.py REPLIB_uncl.fasta > REPLIB_clust.fasta
 
+### running pfam_scan.pl on the clustered REPLIB_clust.fasta, translating it to protein sequences
+conda activate pfam_scan.pl
+pfam_scan.pl -fasta REPLIB_clust.fasta -dir PFAM_lib -e_dom 0.01 -e_seq 0.01 -translate all -outfile pfam_REPLIB.out
+cat pfam_REPLIB.out | python /global/scratch/users/annen/KVKLab/Phase1/parse_pfam.py > pfam_REPLIB_list.txt
+conda deactivate
+
+### running rpsblast on the clustered REPLIB_clust.fasta
+rpstblastn -query REPLIB_clust.fasta -db CDD_lib/CDD_lib -out cdd_REPLIB.out -evalue 0.001 -outfmt 6
+cat cdd_REPLIB.out | python /global/scratch/users/annen/KVKLab/Phase1/parse_cdd.py > cdd_REPLIB_list.txt
+
+### create final clustered library with domains
+cat pfam_REPLIB_list.txt cdd_REPLIB_list.txt | python /global/scratch/users/annen/KVKLab/Phase1/parse_domlib.py REPLIB_clust.fasta > REPLIB_DOM.fasta
 
