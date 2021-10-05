72b3811a335d99d07dee84cc7276c9dd695f5478
Anne Nakamoto <annen@berkeley.edu>
2021-10-04 23:44:25 -0700

    indiv TE dom trees

--- Rep_TE_Lib/indiv_TEdom_trees.sh
+++ Rep_TE_Lib/indiv_TEdom_trees.sh
@@ -9,6 +9,7 @@
 #SBATCH --mail-type=ALL
 
 ###     Create a TE tree based on one domain
+###     Usage: sbatch KVKLab/Rep_TE_Lib/indiv_TEdom_trees.sh <TE> <PFAM domain>
 
 TE=$1   # RepBase element (ex. MAGGY)
 DOM=$2  # pfam domain (ex. RVT_1)
@@ -17,8 +18,10 @@ cd /global/scratch/users/annen/Rep_TE_Lib/Align_TE_Doms
 source activate /global/scratch/users/annen/anaconda3/envs/pfam_scan.pl
 
 ### scan RepeatMasker hits (REPHITS_${TE}.fasta) for DOM using pfam_scan.pl
-pfam_scan.pl -fasta /global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${TE}.fasta -dir /PFAM_libs/${DOM} -e_dom 0.01 -e_seq 0.01 -translate all -outfile ${TE}.pfam.out
+pfam_scan.pl -fasta /global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${TE}.fasta -dir PFAM_libs/${DOM} -e_dom 0.01 -e_seq 0.01 -translate all -outfile ${TE}.pfam.out
 echo "scanned ${TE} TEs for ${DOM} domain"
+
+conda deactivate
 
 ### make a bed file of the pfam_scan.pl output
 awk -v OFS='\t' '$18 ~ !/\#/ { print $1, $2, $3, $7, "0", $16 }' ${TE}.pfam.out > ${TE}.pfam.bed
@@ -26,8 +29,6 @@ awk -v OFS='\t' '$18 ~ !/\#/ { print $1, $2, $3, $7, "0", $16 }' ${TE}.pfam.out 
 ### extract the domain sequences using bedtools getfasta
 bedtools getfasta -fo ${TE}.${DOM}.fasta -name -s -fi /global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${TE}.fasta -bed ${TE}.pfam.bed
 echo "got the ${DOM} domain sequences from each ${TE} TE"
-
-conda deactivate
 
 ### align extracted sequences using MAFFT
 mafft ${TE}.${DOM}.fasta > Alignments/${TE}.${DOM}.afa
