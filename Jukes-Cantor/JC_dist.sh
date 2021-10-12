7ec4e550cdc8f0808aff66604d4b85edc0841c27
Anne Nakamoto <annen@berkeley.edu>
2021-10-11 18:55:57 -0700

    output

--- Jukes-Cantor/JC_dist.sh
+++ Jukes-Cantor/JC_dist.sh
@@ -12,11 +12,13 @@
 
 NUM=$1    ### first argument is the number of sequences in fasta file
 FASTA=$2  ### second argument is the path to fasta file
+file=$(basename $2)
 
 # TEST RUN: sbatch KVKLab/Jukes-Cantor/JC_dist.sh 9 /global/scratch/users/annen/JC_Dist/RepBase_TEs_interest.fasta.txt
-
+module unload python
 source activate /global/scratch/users/annen/anaconda3/envs/Biopython
 
-python KVKLab/Jukes-Cantor/jc_dist_BOYAN.py $NUM $FASTA
+python KVKLab/Jukes-Cantor/jc_dist_BOYAN.py $NUM $FASTA > JC_Dist/${file}_jc.out
 
 conda deactivate
+module load python
