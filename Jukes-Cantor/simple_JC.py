d75d396c91201d18072d77ada52393c1071dc6cd
Anne Nakamoto <annen@berkeley.edu>
2021-10-18 01:24:19 -0700

    ln fract

--- Jukes-Cantor/simple_JC.py
+++ Jukes-Cantor/simple_JC.py
@@ -9,7 +9,7 @@ for line in sys.stdin:
     length = int(lst[1])
     p = (length-identity)/length
     if p >= 3/4:
-        print(te + '\t' + "1")
+        print(te + '\t' + "1" + '\t' + line)
     else:
         jc_dist = -(3/4)*math.log(1-((4/3)*p))
-        print(te + '\t' + str(jc_dist))
+        print(te + '\t' + str(jc_dist) + '\t' + line)
