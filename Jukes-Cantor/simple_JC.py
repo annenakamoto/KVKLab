ed5e13fb47a9b91fba31820b3f5ade4a0312b410
Anne Nakamoto <annen@berkeley.edu>
2021-10-18 01:31:16 -0700

    domain eror

--- Jukes-Cantor/simple_JC.py
+++ Jukes-Cantor/simple_JC.py
@@ -8,7 +8,7 @@ for line in sys.stdin:
     identity = int(lst[0])
     length = int(lst[1])
     p = (length-identity)/length
-    if p >= 3/4:
+    if p < 3/4:
         jc_dist = -(3/4)*math.log(1-((4/3)*p))
         if jc_dist < 0:
             print(te + '\t' + str(jc_dist) + '\t' + line)
