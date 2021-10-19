888cbe448a1444d04c0d15ee726049b6194e68ce
Anne Nakamoto <annen@berkeley.edu>
2021-10-19 13:49:01 -0700

    jc lineages

--- Jukes-Cantor/JC_dist_ref.sh
+++ Jukes-Cantor/JC_dist_ref.sh
@@ -10,13 +10,12 @@
 
 ###     find Jukes-Cantor distances between one reference sequence and sequences in a library file
 
-NAME=$1         ### name of the TE, or "SCOs"
+NAME=$1         ### name of the TE
 DOM=$2          ### name of the domain for filtering
 LIB_PATH=/global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${NAME}.fasta     ### path to fasta file containing library of many sequences
                 ### /global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${TE}.fasta                
 
 ########### I'll use the filtered TE set from the cleaner-looking domain based trees, and the full TE nucleotide sequences
-########### Actually, trying with the full REPHITS libraries first
 
 lib=$(basename $1)
 
@@ -24,18 +23,18 @@ lib=$(basename $1)
 cd /global/scratch/users/annen/JC_dist_filt
 
 ### filter TE library (filtered library in ${NAME}.filt_lib.fasta)
-#cat /global/scratch/users/annen/Rep_TE_Lib/PFAM_lib/1${NAME}.${DOM}.fa_align.Matches.* | awk '/>/ { print substr($1, 1, length($1)-2) }' | tr \# \: | python /global/scratch/users/annen/KVKLab/Jukes-Cantor/filter_te_lib.py ${LIB_PATH} ${NAME} > ${NAME}.filt_lib.fasta
+cat /global/scratch/users/annen/Rep_TE_Lib/PFAM_lib/1${NAME}.${DOM}.fa_align.Matches.* | awk '/>/ { print substr($1, 1, length($1)-2) }' | tr \# \: | python /global/scratch/users/annen/KVKLab/Jukes-Cantor/filter_te_lib.py ${LIB_PATH} ${NAME} > ${NAME}.filt_lib.fasta
 echo "*** created filtered ${NAME} library ***"
 
 ### generate MSA and remove all-gap columns
-#mafft --localpair ${NAME}.filt_lib.fasta > ${NAME}.filt_lib.aligned
-#source activate /global/scratch/users/annen/anaconda3/envs/pfam_scan.pl
-#esl-reformat --mingap -o ${NAME}.filt_al.nogap afa ${NAME}.filt_lib.aligned
-#source deactivate
+mafft --localpair ${NAME}.filt_lib.fasta > ${NAME}.filt_lib.aligned
+source activate /global/scratch/users/annen/anaconda3/envs/pfam_scan.pl
+esl-reformat --mingap -o ${NAME}.filt_al.nogap afa ${NAME}.filt_lib.aligned
+source deactivate
 echo "*** finished ${NAME} MSA ***"
 
 ### generate consensus sequence
-#cons -sequence ${NAME}.filt_al.nogap -outseq ${NAME}.filt.cons.fasta -name ${NAME}
+cons -sequence ${NAME}.filt_al.nogap -outseq ${NAME}.filt.cons.fasta -name ${NAME}
 echo "*** generated ${NAME} consensus sequence ***"
 cat ${NAME}.filt.cons.fasta | awk '{ gsub("n", ""); print; }' > ${NAME}.CONS.fasta 
 echo "*** removed unknown (n) characters from consensus ***"
