87b93ce7c1a2a12382936ed5a937f242bf2fe459
Anne Nakamoto <annen@berkeley.edu>
2021-10-29 21:23:39 -0700

    run gffread

--- Jukes-Cantor/JC_dist_gen.sh
+++ Jukes-Cantor/JC_dist_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ref_jukes_cantor
+#SBATCH --job-name=gen_jukes_cantor
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
@@ -15,16 +15,19 @@
 ### SETUP FILES
 cd /global/scratch/users/annen/JC_gist_genomes
 
-while read orthogroup; do
-    grep $orthogroup Orthogroups.txt
-done < SingleCopyOrthogroups.txt
+# RUN GFFREAD: gffread-0.12.7.Linux_x86_64/gffread -h
+# gffread -g <genome_fasta> -x <coding_seqs_output_fasta> --ids <list_of_ids> <gff_file>
+gffread-0.12.7.Linux_x86_64/gffread -g guy11.fasta -x guy11.cds.fasta guy11.fungap_out.gff3
 
-FunGAP_out/oryza/guy11/fungap_out/fungap_out.gff3
+#while read orthogroup; do
+#    grep $orthogroup Orthogroups.txt
+#done < SingleCopyOrthogroups.txt
+
 
 ### COMPUTE DISTANCES
 
 ### use needle to align each SCO to the reference gene and find the percent identity, then compute JC dist in python
-needle -asequence ${TE}.${GEN}.CONS.fasta  -bsequence ${TE}.${GEN}.filt_lib.fasta -outfile ${TE}.${GEN}.filt.needle -gapopen 10.0 -gapextend 0.5
-echo "*** finished needle for ${TE} ${GEN} ***"
-cat ${TE}.${GEN}.filt.needle | awk '/# Identity:/ { print $3 }' | python /global/scratch/users/annen/KVKLab/Jukes-Cantor/simple_JC.py ${TE}.${GEN} > ${TE}.${GEN}.filt.JC.out.txt
-echo "*** finished computing JC discances ***"
+#needle -asequence ${TE}.${GEN}.CONS.fasta  -bsequence ${TE}.${GEN}.filt_lib.fasta -outfile ${TE}.${GEN}.filt.needle -gapopen 10.0 -gapextend 0.5
+#echo "*** finished needle for ${TE} ${GEN} ***"
+#cat ${TE}.${GEN}.filt.needle | awk '/# Identity:/ { print $3 }' | python /global/scratch/users/annen/KVKLab/Jukes-Cantor/simple_JC.py ${TE}.${GEN} > ${TE}.${GEN}.filt.JC.out.txt
+#echo "*** finished computing JC discances ***"
