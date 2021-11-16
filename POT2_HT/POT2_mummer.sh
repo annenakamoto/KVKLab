54e2e1470a3475c3cb7896b2309b9a421f85a012
Anne Nakamoto <annen@berkeley.edu>
2021-11-16 15:55:08 -0800

    infile

--- POT2_HT/POT2_mummer.sh
+++ POT2_HT/POT2_mummer.sh
@@ -40,7 +40,7 @@ ls guy11_fastas/guy11_POT2* | while read ref; do
         /global/scratch/users/annen/MUMmer/mummer-4.0.0rc1/show-coords nucmer_out/${query_b}.${ref_b}.delta > show_coords_out/${query_b}.${ref_b}.coords
         len=$(cat show_coords_out/${query_b}.${ref_b}.coords | awk '/POT2/ { print $7 }')
         pid=$(cat show_coords_out/${query_b}.${ref_b}.coords | awk '/POT2/ { print $10 }')
-        plot=$(awk -v len=${len} -v pid=${pid} 'BEGIN { if (len >= 30000 && pid >= 80.0) { print "true"; } }')
+        plot=$(echo | awk -v len=${len} -v pid=${pid} 'BEGIN { if (len >= 30000 && pid >= 80.0) { print "true"; } }')
         if [ ${plot} == "true" ]; then
             /global/scratch/users/annen/MUMmer/mummer-4.0.0rc1/mummerplot --postscript --color -p mummerplot_out/${query_b}.${ref_b} nucmer_out/${query_b}.${ref_b}.delta
             ps2pdf mummerplot_out/${query_b}.${ref_b}.ps pdf_plots/${query_b}.${ref_b}.pdf
