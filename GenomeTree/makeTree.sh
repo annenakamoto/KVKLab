01ddb5e78eb6736e5f60a839e5645ce2506c62df
Anne Nakamoto <annen@berkeley.edu>
2021-11-22 18:49:24 -0800

    make tree job

---
+++ GenomeTree/makeTree.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=GenomeTree
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+### Make high quality genome tree
+
+cd /global/scratch/users/annen/GENOME_TREE
+
+source activate /global/scratch/users/annen/anaconda3/envs/Biopython
+echo "*** Processing proteomes ***"
+cat PROTEOMES/genome_list.txt | /global/scratch/users/annen/KVKLab/GenomeTree/process_faa_for_orthofinder.py
+echo "*** Done ***"
+conda deactivate
+
+### Run OrthoFinder
