9fa8005de219f5abca4a7a06802963acbf5004e5
Anne Nakamoto <annen@berkeley.edu>
2021-11-22 19:53:24 -0800

    orthofinder

--- GenomeTree/makeTree.sh
+++ GenomeTree/makeTree.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=ALL
 
@@ -12,10 +12,13 @@
 
 cd /global/scratch/users/annen/GENOME_TREE
 
-source activate /global/scratch/users/annen/anaconda3/envs/Biopython
-echo "*** Processing proteomes ***"
-cat PROTEOMES/genome_list.txt | python /global/scratch/users/annen/KVKLab/GenomeTree/process_faa_for_orthofinder.py
-echo "*** Done ***"
-conda deactivate
+#source activate /global/scratch/users/annen/anaconda3/envs/Biopython
+#echo "*** Processing proteomes ***"
+#cat PROTEOMES/genome_list.txt | python /global/scratch/users/annen/KVKLab/GenomeTree/process_faa_for_orthofinder.py
+#echo "*** Done ***"
+#conda deactivate
 
 ### Run OrthoFinder
+source activate /global/scratch/users/annen/anaconda3/envs/OrthoFinder
+orthofinder -oa -f PROTEOMES_filt -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -o OrthoFinder_out
+conda deactivate
