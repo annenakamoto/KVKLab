8d9223adc07342c4af986af1296173706d7f7672
Anne Nakamoto <annen@berkeley.edu>
2022-01-24 22:00:59 -0800

    naming

--- Effector_analysis/effector_analysis.sh
+++ Effector_analysis/effector_analysis.sh
@@ -16,24 +16,24 @@ GENOME=$1
 ### SignalP
 ./signalp -fasta /global/scratch/users/annen/Effector_analysis/${GENOME}.faa -prefix signalp_${GENOME} #-t euk -u 0.34 -U 0.34
 
-awk '{if ($2 == "SP(Sec/SPI)") {print $1}}' signalp_${GENOME}_summary.signalp5 > signalp_secrete_points_names
-awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' ${GENOME}.faa > ${GENOME}.singleline.faa
+awk '{if ($2 == "SP(Sec/SPI)") {print $1}}' signalp_${GENOME}_summary.signalp5 > signalp_secrete_points_names_${GENOME}
+awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' /global/scratch/users/annen/Effector_analysis/${GENOME}.faa > ${GENOME}.singleline.faa
 
 > ${GENOME}_signalp_proteins.faa
 while read gene; do
     grep -A1 ${gene} ${GENOME}.singleline.faa >> ${GENOME}_signalp_proteins.faa
-done < signalp_secrete_points_names
+done < signalp_secrete_points_names_${GENOME}
 
 cd /global/scratch/users/annen/Effector_analysis
 
 ### tmhmm
-tmhmm-2.0c/bin/tmhmm signalp-5.0b/bin/${GENOME}_signalp_proteins.faa > tmhmm_output
+tmhmm-2.0c/bin/tmhmm signalp-5.0b/bin/${GENOME}_signalp_proteins.faa > tmhmm_output_${GENOME}
 
-grep "Number" tmhmm_output | awk '{if ($7 == 0){print $2}} ' > ${GENOME}_signalp_notmhmm_protein_names
+grep "Number" tmhmm_output_${GENOME} | awk '{if ($7 == 0){print $2}} ' > ${GENOME}_signalp_notmhmm_protein_names
 
 > ${GENOME}_signalp_notmhmm_proteins.faa
 while read gene; do
-    grep -A1 ${gene} ${GENOME}_fungap_out_prot.singleline.faa >> ${GENOME}_signalp_notmhmm_proteins.faa
+    grep -A1 ${gene} signalp-5.0b/bin/${GENOME}.singleline.faa >> ${GENOME}_signalp_notmhmm_proteins.faa
 done < ${GENOME}_signalp_notmhmm_protein_names
 
 ### EffectorP
