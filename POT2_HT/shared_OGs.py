14497d3528eadc5d692fca1fb8589f3c48e2d7d2
Anne Nakamoto <annen@berkeley.edu>
2022-02-03 11:28:41 -0800

    debug

--- POT2_HT/shared_OGs.py
+++ POT2_HT/shared_OGs.py
@@ -44,6 +44,7 @@ for pot2_g, genes_g in GUY11.items():
 ### print in order of most shared genes to least
 SORTED_G = sorted(SHARED_GENES.keys(), key=lambda x: len(SHARED_GENES[x]), reverse=True)
 for key in SORTED_G:
+    print(key)
     g, b = re.findall('\d\.\d{6}', key)
     if abs(float(g) - float(b)) < 0.3:
         print('\t'.join([str(len(SHARED_GENES[key])), key, ';'.join(SHARED_GENES[key])]))
