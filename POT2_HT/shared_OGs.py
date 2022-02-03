60f55eb969108e4ba66a91308443f7f3d4b0e0d9
Anne Nakamoto <annen@berkeley.edu>
2022-02-03 09:15:28 -0800

    fixed

--- POT2_HT/shared_OGs.py
+++ POT2_HT/shared_OGs.py
@@ -19,8 +19,6 @@ with open(guy11_intersect, 'r') as guy11:
             GUY11[pot2] = [gene]
         else:
             GUY11[pot2].append(gene)
-        if not GUY11[pot2]:
-            print(pot2, gene)
 
 B71 = {}
 with open(B71_intersect, 'r') as b71:
@@ -33,15 +31,11 @@ with open(B71_intersect, 'r') as b71:
             B71[pot2] = [gene]
         else:
             B71[pot2].append(gene)
-        if not B71[pot2]:
-            print(pot2, gene)
 
 ### Dictionary containing: key = <[pot2 in guy11, pot2 in b71]>, value = <list of genes in common>
 SHARED_GENES = {}
 for pot2_g, genes_g in GUY11.items():
     for pot2_b, genes_b in B71.items():
-        print(genes_g, genes_b)
-        print(pot2_g, pot2_b)
         overlap = list(set(genes_g) & set(genes_b))
         if overlap:
             SHARED_GENES[list(pot2_g, pot2_b)] = overlap
@@ -49,4 +43,4 @@ for pot2_g, genes_g in GUY11.items():
 ### print in order of most shared genes to least
 SORTED_G = sorted(SHARED_GENES.keys(), key=lambda x: len(SHARED_GENES[x]), reverse=True)
 for key in SORTED_G:
-    print('\t'.join(len(SORTED_G[key]), key[0], key[1], ';'.join(SORTED_G[key])))
+    print('\t'.join(str(len(SORTED_G[key])), key[0], key[1], ';'.join(SORTED_G[key])))
