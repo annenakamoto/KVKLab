a23a216d13de2202935da93e56e6a9c4d200dfbc
Anne Nakamoto <annen@berkeley.edu>
2022-02-03 08:30:08 -0800

    pot2 flank

--- POT2_HT/POT2_flank.sh
+++ POT2_HT/POT2_flank.sh
@@ -13,11 +13,20 @@
 
 cd /global/scratch/users/annen/POT2_flank
 
+### make chrom length file for slop
+cat /global/scratch/users/annen/JC_cons_genomes/guy11.fasta | python /global/scratch/users/annen/KVKLab/POT2_HT/chrom_len.py > guy11.len
+cat /global/scratch/users/annen/JC_cons_genomes/B71.fasta | python /global/scratch/users/annen/KVKLab/POT2_HT/chrom_len.py > B71.len
+
 ### Get POT2 coordinates + 50,000 bp flank on each side
 bedtools slop -i POT2.guy11.bed -g guy11.len -b 50000 > guy11.POT2_flank.bed
 bedtools slop -i POT2.B71.bed -g B71.len -b 50000 > B71.POT2_flank.bed
 
 ### Grab all genes in that region OG_guy11.bed
-bedtools intersect
+bedtools intersect -a guy11.POT2_flank.bed -b OG_guy11.bed -wa -wb > guy11.POT2_genes.bed
+bedtools intersect -a B71.POT2_flank.bed -b OG_B71.bed -wa -wb > B71.POT2_genes.bed
 
-### 
+### Find POT2 in guy11 and B71 that have the most genes in common (rank)
+python /global/scratch/users/annen/KVKLab/POT2_HT/shared_OGs.py guy11.POT2_genes.bed B71.POT2_genes.bed > ranked_guy11_B71_POT2.txt
+
+
+
---
+++ POT2_HT/shared_OGs.py
@@ -0,0 +1,44 @@
+import sys
+
+### Parse guy11 intersect file (argv[1]) and B71 intersect file (argv[2])
+### 
+
+guy11_intersect = sys.argv[1]
+B71_intersect = sys.argv[2]
+
+### Dictionary structure: key = <POT2 identifier>, value = <list of flanking genes>
+
+GUY11 = {}
+with open(guy11_intersect, 'r') as guy11:
+    for line in guy11:
+        lst = line.split()
+        pot2 = '\t'.join(lst[0:4])
+        gene = lst[12]
+        if pot2 not in GUY11.keys():
+            GUY11[pot2] = [gene]
+        else:
+            GUY11[pot2] = GUY11[pot2].append(gene)
+
+B71 = {}
+with open(B71_intersect, 'r') as b71:
+    for line in b71:
+        lst = line.split()
+        pot2 = '\t'.join(lst[0:4])
+        gene = lst[12]
+        if pot2 not in GUY11.keys():
+            B71[pot2] = [gene]
+        else:
+            B71[pot2] = B71[pot2].append(gene)
+
+### Dictionary containing: key = <[pot2 in guy11, pot2 in b71]>, value = <list of genes in common>
+SHARED_GENES = {}
+for pot2_g, genes_g in GUY11.items():
+    for pot2_b, genes_b in B71.items():
+        overlap = list(set(genes_g) & set(genes_b))
+        if overlap:
+            SHARED_GENES[list(pot2_g, pot2_b)] = overlap
+
+### print in order of most shared genes to least
+SORTED_G = sorted(SHARED_GENES.keys(), key=lambda x: len(SHARED_GENES[x]), reverse=True)
+for key in SORTED_G:
+    print('\t'.join(len(SORTED_G[key]), key[0], key[1], ';'.join(SORTED_G[key])))
