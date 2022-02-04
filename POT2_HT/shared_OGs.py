d948a79abb6e32f33f1f5a558f4e423263bd2ef7
Anne Nakamoto <annen@berkeley.edu>
2022-02-03 23:19:58 -0800

    0.2 max jc

--- POT2_HT/shared_OGs.py
+++ POT2_HT/shared_OGs.py
@@ -47,5 +47,5 @@ for key in SORTED_G:
     gb = re.findall('\d\.\d{6}', key)
     if len(gb) > 2:
         print("ERROR!")
-    if len(gb) == 2 and abs(float(gb[0]) - float(gb[1])) < 0.03:
+    if len(gb) == 2 and abs(float(gb[0]) - float(gb[1])) < 0.03 and float(gb[0]) < 0.2 and float(gb[1]) < 0.2:
         print('\t'.join([str(len(SHARED_GENES[key])), key, ';'.join(SHARED_GENES[key])]))
