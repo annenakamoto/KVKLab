b9ab38b79b53c46c559d1ade345a1bad140bd9f2
Anne Nakamoto <annen@berkeley.edu>
2022-02-26 17:32:36 -0800

    cactus

--- BEAST_analysis/intergenic_align.sh
+++ BEAST_analysis/intergenic_align.sh
@@ -13,15 +13,19 @@
 cd /global/scratch/users/annen/GENOME_TREE
 
 ### make intergenic sequences bedfile (complement of genic bed)
-while read GENOME; do
-    ### Generate chromosome length file
-    cat hq_genomes/${GENOME}.fasta | python /global/scratch/users/annen/KVKLab/POT2_HT/chrom_len.py > LEN/${GENOME}.len
-    bedtools complement -i SCO_BED/SCO_${GENOME}.bed -g LEN/${GENOME}.len > INTERGENIC/int_${GENOME}.bed
-    echo "***finished ${GENOME}***"
-done < genome_list_no_out.txt
+# while read GENOME; do
+#     ### Generate chromosome length file
+#     cat hq_genomes/${GENOME}.fasta | python /global/scratch/users/annen/KVKLab/POT2_HT/chrom_len.py > LEN/${GENOME}.len
+#     bedtools complement -i SCO_BED/SCO_${GENOME}.bed -g LEN/${GENOME}.len > INTERGENIC/int_${GENOME}.bed
+#     echo "***finished ${GENOME}***"
+# done < genome_list_no_out.txt
 
 ### now what to do with intergenic regions bedfiles? Do genome-wide alignments to pick out the syntenic regions, then do alignments 
 
 
-### genome-wide alignments to get syntenic regions (bedfile?)
-### 
+### genome-wide alignments (of full genomes) to get syntenic regions (bedfile?)
+cd /global/scratch/users/annen/Cactus_analysis
+source activate /global/scratch/users/annen/anaconda3/envs/cactus
+cactus --workDir WORKDIR JOBSTORE moryzae_seqFile.txt moryzae_wgalign.cactus.hal
+conda deactivate
+
