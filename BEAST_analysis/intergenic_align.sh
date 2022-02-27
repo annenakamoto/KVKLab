8eabcbd2a890d8a4a61a2d681effc601d60b3efb
Anne Nakamoto <annen@berkeley.edu>
2022-02-26 17:49:03 -0800

    cactus jbstr

--- BEAST_analysis/intergenic_align.sh
+++ BEAST_analysis/intergenic_align.sh
@@ -27,6 +27,7 @@ cd /global/scratch/users/annen/GENOME_TREE
 cd /global/scratch/users/annen/Cactus_analysis
 source activate /global/scratch/users/annen/anaconda3/envs/cactus
 export PATH=$PATH:/global/scratch/users/annen/cactus-bin-v2.0.5/bin
-cactus --binariesMode local --workDir WORKDIR JOBSTORE moryzae_seqFile.txt moryzae_wgalign.cactus.hal
+d=$(date +"%m-%d-%y_%T")
+cactus --binariesMode local --workDir WORKDIR_${d} JOBSTORE moryzae_seqFile.txt moryzae_wgalign.cactus.hal
 conda deactivate
 
