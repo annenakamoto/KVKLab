898ed386c484ea4b5c531745a670398677ac7f53
Anne Nakamoto <annen@berkeley.edu>
2022-03-01 23:44:51 -0800

    no duplicates

--- LTR_divergence/name2bed.py
+++ LTR_divergence/name2bed.py
@@ -5,11 +5,14 @@ import sys
 ###     example: >MAGGY_I:UCNY03000025.1:94032-98838(+):US71
 ###              UCNY03000025.1  94032   98838   MAGGY_I   0   +
 
+HEADERS = {}
 for header in sys.stdin:
-    lst = header.split(":")
-    tmp = lst[2].split("(")
-    coords = tmp[0].split("-")
-    start = coords[0]
-    end = coords[1]
-    strand = tmp[1][0]
-    print(lst[1] + "\t" + start + "\t" + end + "\t" + lst[0][1:] + "\t" + "0" + "\t" + strand)
+    if not HEADERS.get(header, False):
+        HEADERS[header] = True
+        lst = header.split(":")
+        tmp = lst[2].split("(")
+        coords = tmp[0].split("-")
+        start = coords[0]
+        end = coords[1]
+        strand = tmp[1][0]
+        print(lst[1] + "\t" + start + "\t" + end + "\t" + lst[0][1:] + "\t" + "0" + "\t" + strand)
