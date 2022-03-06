b9f74b591f1ebdb13c1a2ee6c2ae501b73890a54
Anne Nakamoto <annen@berkeley.edu>
2022-03-06 15:01:05 -0800

    fix flank coords

--- LTR_divergence/filterLTRs.py
+++ LTR_divergence/filterLTRs.py
@@ -25,8 +25,8 @@ for k,v in LTR.items():
         if e_strand != k_strand:
             LTR[k].remove(e)
     if len(v) >= 2:
-        k_left = int(k.split()[1])
-        k_right = int(k.split()[2])
+        k_left = int(k.split()[1])+1000     # correct for the 1000 bp added to each side using slop
+        k_right = int(k.split()[2])-1000
         left = min(v, key=lambda x:abs(int(x.split()[1])-k_left))
         right = min(v, key=lambda x:abs(int(x.split()[2])-k_right))
         if left != right:
@@ -38,56 +38,56 @@ for v in LTR_PAIRS.values():
     ltrs += v
 ltrs_set = set(ltrs)
 duplicates = []
-# print("LENGTHS: ", len(ltrs),  len(ltrs_set))
+print("LENGTHS: ", len(ltrs),  len(ltrs_set))
 if len(ltrs) != len(ltrs_set):
-    # print("THERE ARE DUPLICATE LTRS")
+    print("THERE ARE DUPLICATE LTRS")
     for ltr in ltrs_set:
         c = ltrs.count(ltr)
         if c > 1:
-            # print("count: ", c, ltr)
+            print("count: ", c, ltr)
             duplicates.append(ltr)
             
 ### handle duplicates: only keep the best fit
 for ltr in duplicates:
-    # print("HANDLING DUPLICATE: ", ltr)
+    print("HANDLING DUPLICATE: ", ltr)
     dup = []
     for k,v in LTR_PAIRS.items():
         if ltr in v:
             dup.append(k)
     if len(dup) == 2:
-        # print("keep one of:")
-        # print("0: ", dup[0])
-        # print("1: ", dup[1])
-        zero_overlap = abs(max(int(dup[0].split()[1]), int(ltr.split()[1])) - min(int(dup[0].split()[2]), int(ltr.split()[2])))
-        one_overlap = abs(max(int(dup[1].split()[1]), int(ltr.split()[1])) - min(int(dup[1].split()[2]), int(ltr.split()[2])))
-        # print("0 overlap: ", zero_overlap)
-        # print("1 overlap: ", one_overlap)
+        print("keep one of:")
+        print("0: ", dup[0])
+        print("1: ", dup[1])
+        zero_overlap = abs(max(int(dup[0].split()[1]), int(ltr.split()[1])+1000) - min(int(dup[0].split()[2]), int(ltr.split()[2])-1000))
+        one_overlap = abs(max(int(dup[1].split()[1]), int(ltr.split()[1])+1000) - min(int(dup[1].split()[2]), int(ltr.split()[2])-1000))
+        print("0 overlap: ", zero_overlap)
+        print("1 overlap: ", one_overlap)
         if zero_overlap == one_overlap:
-            # print("0 and 1 dist same?? removing both")
+            print("0 and 1 dist same?? removing both")
             LTR_PAIRS.pop(dup[0])
             LTR_PAIRS.pop(dup[1])
         elif zero_overlap > one_overlap:
-            # print("kept 0")
+            print("kept 0")
             LTR_PAIRS.pop(dup[1])
         else:
-            # print("kept 1")
+            print("kept 1")
             LTR_PAIRS.pop(dup[0])
 
 ### return output 
 ### rename the LTRs
-c = 1
-with open(sys.argv[1], 'w') as f:
-    for k,v in LTR_PAIRS.items():
-        v0 = v[0].split()
-        v1 = v[1].split()
-        n0 = v0[3].split("_")[0] + "_LTR_" + str(c) + "a"
-        n1 = v1[3].split("_")[0] + "_LTR_" + str(c) + "b"
-        v0[3] = n0
-        v1[3] = n1
-        print('\t'.join(v0))
-        print('\t'.join(v1))
-        f.write(str(c) + ": " + k + '\n') # keep track of what number referrs to which internal region
-        c += 1
+# c = 1
+# with open(sys.argv[1], 'w') as f:
+#     for k,v in LTR_PAIRS.items():
+#         v0 = v[0].split()
+#         v1 = v[1].split()
+#         n0 = v0[3].split("_")[0] + "_LTR_" + str(c) + "a"
+#         n1 = v1[3].split("_")[0] + "_LTR_" + str(c) + "b"
+#         v0[3] = n0
+#         v1[3] = n1
+#         print('\t'.join(v0))
+#         print('\t'.join(v1))
+#         f.write(str(c) + ": " + k + '\n') # keep track of what number referrs to which internal region
+#         c += 1
 
 
 
