e6a2d652e8581951a334f65fa7ff11f63c5a4b37
Anne Nakamoto <annen@berkeley.edu>
2022-03-06 20:16:35 -0800

    calculate div

--- LTR_divergence/identity.py
+++ LTR_divergence/identity.py
@@ -4,6 +4,7 @@ import sys
 ### sys.argv[1]: name of LTR pair, eg. "guy11.Copia_LTR_14"
 
 name = sys.argv[1].split(".")[1]
+genome = sys.argv[1].split(".")[0]
 ltr = name.split("_")[0] + "_" + name.split("_")[1]
 number = name.split("_")[2]
 
@@ -13,6 +14,6 @@ for line in sys.stdin:
     num = int(lst2[0])
     den = int(lst2[1])
     div = 1 - (num/den)
-    result = [ltr, number, str(div)]
+    result = [ltr, number, genome, str(div)]
     print('\t'.join(result))
     break
