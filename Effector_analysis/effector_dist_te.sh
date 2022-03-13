e43c2f9c39aead0e9c014762ece682f00cce1e45
Anne Nakamoto <annen@berkeley.edu>
2022-03-13 16:49:59 -0700

    effector dist to te

---
+++ Effector_analysis/effector_dist_te.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=effector_dist_TE
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+### Find the 5' (upstream) and 3' (downstream) distances of genes/effectors to TEs
+###     Gene bedfiles are:      visualize_OGs/E_SCO_OG_${GENOME}.bed
+###     Effector bedfiles are:  visualize_OGs/EFF_${GENOME}.bed
+###     TE bedfiles are:        visualize_TEs/${TE}.${GENOME}.bed
+
+cd /global/scratch/users/annen/Effector_dist_TE
+
+while read GENOME; do
+    ### Find the distance of ALL genes in GENOME to the nearest TE (any)
+    echo "*** STARTING ${GENOME} ***"
+    echo "*** ${GENOME}: all genes to all TEs ***"
+    cat /global/scratch/users/annen/visualize_TEs/*${GENOME}.bed > all_TEs.${GENOME}.bed
+    bedtools closest -D a -fu -t first -a /global/scratch/users/annen/visualize_OGs/E_SCO_OG_${GENOME}.bed -b all_TEs.${GENOME}.bed > genes_TEs.U.${GENOME}.bed
+    bedtools closest -D a -fd -t first -a /global/scratch/users/annen/visualize_OGs/E_SCO_OG_${GENOME}.bed -b all_TEs.${GENOME}.bed > genes_TEs.D.${GENOME}.bed
+
+    ### parse the data
+
+    ### Find the distance of effectors in GENOME to the nearest individual TE (the ones that had expansions in that genome)
+    while read TE; do
+        echo "*** ${GENOME}: all effectors to ${TE} ***"
+        bedtools closest -D a -fu -t first -a /global/scratch/users/annen/visualize_OGs/EFF_${GENOME}.bed -b /global/scratch/users/annen/visualize_TEs/${TE}.${GENOME}.bed > eff_${TE}.U.${GENOME}.bed
+        bedtools closest -D a -fd -t first -a /global/scratch/users/annen/visualize_OGs/EFF_${GENOME}.bed -b /global/scratch/users/annen/visualize_TEs/${TE}.${GENOME}.bed > eff_${TE}.D.${GENOME}.bed
+    
+        ### parse the data
+    
+    done < te_list.txt 
+done < genome_list.txt
+
+
