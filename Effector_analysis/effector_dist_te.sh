ebb31a09ac16021652727308d956744848f02d76
Anne Nakamoto <annen@berkeley.edu>
2022-03-13 17:35:15 -0700

    parse data:

--- Effector_analysis/effector_dist_te.sh
+++ Effector_analysis/effector_dist_te.sh
@@ -12,6 +12,8 @@
 ###     Gene bedfiles are:      visualize_OGs/E_SCO_OG_${GENOME}.bed
 ###     Effector bedfiles are:  visualize_OGs/EFF_${GENOME}.bed
 ###     TE bedfiles are:        visualize_TEs/${TE}.${GENOME}.bed
+###     Output: gene    x-axis          y-axis
+###     Output: gene    upstream(5')    downstream(3')
 
 cd /global/scratch/users/annen/Effector_dist_TE
 
@@ -25,6 +27,12 @@ while read GENOME; do
     bedtools closest -D a -iu -t first -a SORTED/E_SCO_OG_${GENOME}.bed -b SORTED/all_TEs.${GENOME}.bed > genes_TEs.D.${GENOME}.bed
 
     ### parse the data
+    > genes_TEs.${GENOME}.DATA.txt
+    cat genes_TEs.U.${GENOME}.bed | awk '{ print $4 }' | while read GENE; do
+        us=$(grep ${GENE} genes_TEs.U.${GENOME}.bed | awk '{ print -$14 }')
+        ds=$(grep ${GENE} genes_TEs.D.${GENOME}.bed | awk '{ print $14 }')
+        echo -e "${GENE}\t${us}\t${ds}" >> genes_TEs.${GENOME}.DATA.txt
+    done
 
     ### Find the distance of effectors in GENOME to the nearest individual TE (the ones that had expansions in that genome)
     while read TE; do
@@ -35,7 +43,12 @@ while read GENOME; do
         bedtools closest -D a -iu -t first -a SORTED/EFF_${GENOME}.sorted.bed -b SORTED/${TE}.${GENOME}.sorted.bed > eff_${TE}.D.${GENOME}.bed
     
         ### parse the data
-    
+        > eff_${TE}.${GENOME}.DATA.txt
+        cat eff_${TE}.U.${GENOME}.bed | awk '{ print $4 }' | while read GENE; do
+            us=$(grep ${GENE} eff_${TE}.U.${GENOME}.bed | awk '{ print -$14 }')
+            ds=$(grep ${GENE} eff_${TE}.D.${GENOME}.bed | awk '{ print $14 }')
+            echo -e "${GENE}\t${us}\t${ds}" >> eff_${TE}.${GENOME}.DATA.txt
+        
     done < te_list.txt 
 done < genome_list.txt
 
