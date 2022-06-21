3b02f0e5b29a96947f342ecf5bb3d0a37c80e047
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-06-21 09:06:14 -0700

    add 70-15 to orthofinder

--- Effector_map_70-15/add_70-15_OF.sh
+++ Effector_map_70-15/add_70-15_OF.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=orthofinder_genetrees
+#SBATCH --job-name=add_70-15
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
@@ -20,7 +20,7 @@ source activate /global/scratch/users/annen/anaconda3/envs/OrthoFinder
 #orthofinder -oa -f PROTEOMES_filt -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -o OrthoFinder_out
 
 ### Use blast results from previous run and add the 70-15 proteome
-orthofinder -b OrthoFinder_out/Results_Nov22 -f 70-15_PROTEOME -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -o OrthoFinder_out
+orthofinder -b OrthoFinder_out/Results_Nov22 -f 70-15_PROTEOME -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -o OrthoFinder_70-15_out
 
 conda deactivate
 echo "*****DONE*****"
