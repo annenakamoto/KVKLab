8ce95417d6c53e18a56f8af73887005aab794983
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-06-23 09:36:56 -0700

    midpoint root treeKO

--- Gene_tree_topology/run_treeKO.sh
+++ Gene_tree_topology/run_treeKO.sh
@@ -23,23 +23,23 @@ path_to_OF_genome_tree="/global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/
 path_to_OF_gene_trees="/global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Gene_Trees"
 
 # prune genome tree
-echo "*** prune the genome tree ***"
-cat ${path_to_OF_genome_tree} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/prune_genome_tree.py "guy11 US71 B71 LpKY97 MZ5-1-6 NI907" > OF_RENAMED/GenomeTree.txt
-echo "*** done ***"
+# echo "*** prune the genome tree ***"
+# cat ${path_to_OF_genome_tree} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/prune_genome_tree.py "guy11 US71 B71 LpKY97 MZ5-1-6 NI907" > OF_RENAMED/GenomeTree.txt
+# echo "*** done ***"
 
-> gene_tree_list.txt
-ls ${path_to_OF_gene_trees} | while read OG; do
-    # Keeps only the leaves of the specified species
-    cat ${path_to_OF_gene_trees}/${OG} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/prune_gene_tree.py "guy11 US71 B71 LpKY97 MZ5-1-6 NI907" > OF_RENAMED/${OG}
-    if [ -s OF_RENAMED/${OG} ]; then
-        echo "OF_RENAMED/${OG}" >> gene_tree_list.txt
-        echo "*** pruned ${OG} ***"
-    else    # remove pruned tree file if empty and don't add to list
-        rm OF_RENAMED/${OG}
-        echo "*** pruned ${OG}, now empty, removing ***"
-    fi
-done
-echo "*** done pruning ***"
+# > gene_tree_list.txt
+# ls ${path_to_OF_gene_trees} | while read OG; do
+#     # Keeps only the leaves of the specified species
+#     cat ${path_to_OF_gene_trees}/${OG} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/prune_gene_tree.py "guy11 US71 B71 LpKY97 MZ5-1-6 NI907" > OF_RENAMED/${OG}
+#     if [ -s OF_RENAMED/${OG} ]; then
+#         echo "OF_RENAMED/${OG}" >> gene_tree_list.txt
+#         echo "*** pruned ${OG} ***"
+#     else    # remove pruned tree file if empty and don't add to list
+#         rm OF_RENAMED/${OG}
+#         echo "*** pruned ${OG}, now empty, removing ***"
+#     fi
+# done
+# echo "*** done pruning ***"
 
 genome_tree="OF_RENAMED/GenomeTree.txt" # path to genome tree to use for treeKO
 gene_tree_list="gene_tree_list.txt"   # text file with list of paths to the gene trees
@@ -47,8 +47,8 @@ gene_tree_list="gene_tree_list.txt"   # text file with list of paths to the gene
 ### create the config file for TreeKO
 > config_file.txt
 echo -e "orto_mode\ts" >> config_file.txt   # s: Orthology and paralogy nodes will be predicted using the species overlap algorithm
-echo -e "root_method\ts" >> config_file.txt # s: root at a user defined species or protein
-echo -e "root_species\tNI9" >> config_file.txt  # specify the species to root trees at
+echo -e "root_method\tm" >> config_file.txt # s: root at the midpoint
+#echo -e "root_species\tNI9" >> config_file.txt  # specify the species to root trees at
 echo -e "print_strict_distance" >> config_file.txt  # only print strict distance
 
 ### run TreeKO
