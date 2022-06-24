b65fe33860e4e1fe3122b7797e472e92ea4012ed
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-06-24 14:59:09 -0700

    gene dist te and strict distance

--- Gene_tree_topology/gene_dist_TE.sh
+++ Gene_tree_topology/gene_dist_TE.sh
@@ -14,33 +14,33 @@
 
 cd /global/scratch/users/annen/treeKO_analysis
 
-
+GENOME=$1
 # contains the positions of genes: /global/scratch/users/annen/visualize_OGs/${GENOME}.bed
 #   gene names are original ones from fungap, need to be mapped to names in orthofinder output (gene_00001 -> gene_0_guy11)
 
-while read GENOME; do    
-    ### Find the distance of genes in GENOME to the nearest individual TE
-    while read TE; do
-        echo "*** ${GENOME}: genes to ${TE} ***"
-        cat /global/scratch/users/annen/visualize_OGs/${GENOME}.bed | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/rename_genes.py ${GENOME} > UNSORTED/GENES_${GENOME}.bed
-        sort -k1,1 -k2,2n UNSORTED/GENES_${GENOME}.bed > SORTED/GENES_${GENOME}.sorted.bed
-        sort -k1,1 -k2,2n /global/scratch/users/annen/visualize_TEs/${TE}.${GENOME}.bed > SORTED/${TE}.${GENOME}.sorted.bed
-        bedtools closest -D a -id -t first -a SORTED/GENES_${GENOME}.sorted.bed -b SORTED/${TE}.${GENOME}.sorted.bed > UPSTREAM/gene_${TE}.U.${GENOME}.bed
-        bedtools closest -D a -iu -t first -a SORTED/GENES_${GENOME}.sorted.bed -b SORTED/${TE}.${GENOME}.sorted.bed > DOWNSTREAM/gene_${TE}.D.${GENOME}.bed
-        ### parse the data
-        echo "*** ${GENOME}: parsing data for genes to ${TE} ***"
-        > gene_${TE}.${GENOME}.DATA.txt
-        cat UPSTREAM/gene_${TE}.U.${GENOME}.bed | awk -v OFS='\t' '{ print $1, $2, $3, $4 }' | while read LINE; do
-            gene=$(echo ${LINE} | awk '{ print $4 }')
-            us=$(grep "${LINE}" UPSTREAM/gene_${TE}.U.${GENOME}.bed | awk '$14 == "-1" { print "none" } $14 != "-1" { print -$14 }')
-            ds=$(grep "${LINE}" DOWNSTREAM/gene_${TE}.D.${GENOME}.bed | awk '$14 == "-1" { print "none" } $14 != "-1" { print $14 }')
-            OG=$(grep "${gene}" /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups.txt | awk -v FS=':' '{ print $1; }')
-            SCO=$(grep "${OG}" /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups_SingleCopyOrthologues.txt | awk '/OG/ { print "SCO"; } !/OG/ { print "no"; }')
-            EFF=$(grep "${gene}" /global/scratch/users/annen/Effector_analysis/${GENOME}_effector_protein_names | awk '/gene/ { print "EFF"; } !/gene/ { print "no"; }')
-            SD=$(grep ${OG} /global/scratch/users/annen/treeKO_analysis/treeKO_output_table.txt | awk '/OG/ { print $2; } !/OG/ { print "none"; }')
-            echo -e "${gene}\t${us}\t${ds}\t${OG}\t${SCO}\t${EFF}\t${SD}" >> gene_${TE}.${GENOME}.DATA.txt
-            ### columns: gene_name, upstream_dist_TE, downstream_dist_TE, Orthogrup, SCO?, EFF?, strict_distance, congruent_or_incongruent?
-        done
-    done < te_list.txt 
-done < genome_list.txt
+   
+### Find the distance of genes in GENOME to the nearest individual TE
+while read TE; do
+    echo "*** ${GENOME}: genes to ${TE} ***"
+    cat /global/scratch/users/annen/visualize_OGs/${GENOME}.bed | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/rename_genes.py ${GENOME} > UNSORTED/GENES_${GENOME}.bed
+    sort -k1,1 -k2,2n UNSORTED/GENES_${GENOME}.bed > SORTED/GENES_${GENOME}.sorted.bed
+    sort -k1,1 -k2,2n /global/scratch/users/annen/visualize_TEs/${TE}.${GENOME}.bed > SORTED/${TE}.${GENOME}.sorted.bed
+    bedtools closest -D a -id -t first -a SORTED/GENES_${GENOME}.sorted.bed -b SORTED/${TE}.${GENOME}.sorted.bed > UPSTREAM/gene_${TE}.U.${GENOME}.bed
+    bedtools closest -D a -iu -t first -a SORTED/GENES_${GENOME}.sorted.bed -b SORTED/${TE}.${GENOME}.sorted.bed > DOWNSTREAM/gene_${TE}.D.${GENOME}.bed
+    ### parse the data
+    echo "*** ${GENOME}: parsing data for genes to ${TE} ***"
+    > gene_${TE}.${GENOME}.DATA.txt
+    cat UPSTREAM/gene_${TE}.U.${GENOME}.bed | awk -v OFS='\t' '{ print $1, $2, $3, $4 }' | while read LINE; do
+        gene=$(echo ${LINE} | awk '{ print $4 }')
+        us=$(grep "${LINE}" UPSTREAM/gene_${TE}.U.${GENOME}.bed | awk '$14 == "-1" { print "none" } $14 != "-1" { print -$14 }')
+        ds=$(grep "${LINE}" DOWNSTREAM/gene_${TE}.D.${GENOME}.bed | awk '$14 == "-1" { print "none" } $14 != "-1" { print $14 }')
+        OG=$(grep "${gene}" /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups.txt | awk -v FS=':' '{ print $1; }')
+        SCO=$(grep "${OG}" /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups_SingleCopyOrthologues.txt | awk '/OG/ { print "SCO"; } !/OG/ { print "no"; }')
+        EFF=$(grep "${gene}" /global/scratch/users/annen/Effector_analysis/${GENOME}_effector_protein_names | awk '/gene/ { print "EFF"; } !/gene/ { print "no"; }')
+        SD=$(grep ${OG} /global/scratch/users/annen/treeKO_analysis/treeKO_output_table.txt | awk '/OG/ { print $2; } !/OG/ { print "none"; }')
+        echo -e "${gene}\t${us}\t${ds}\t${OG}\t${SCO}\t${EFF}\t${SD}" >> gene_${TE}.${GENOME}.DATA.txt
+        ### columns: gene_name, upstream_dist_TE, downstream_dist_TE, Orthogrup, SCO?, EFF?, strict_distance, congruent_or_incongruent?
+    done
+done < te_list.txt 
+
 
