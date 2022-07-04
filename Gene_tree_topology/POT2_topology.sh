775e37e2443056db7502f4bf2da79d791fa835ef
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-04 00:23:53 -0700

    POT2 topology

---
+++ Gene_tree_topology/POT2_topology.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=POT2_topology
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+### Check for gene trees that follow a POT2 HT topology
+
+cd /global/scratch/users/annen/treeKO_analysis
+module unload python
+
+source activate /global/scratch/users/annen/anaconda3/envs/treeKO   # to use ete2 python module
+
+> POT2_topo.DATA.txt
+ls ROOTED | while read TREE; do
+    cat ROOTED/${TREE} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/is_POT2_topo.py ${TREE} >> POT2_topo.DATA.txt
+done
+
+conda deactivate
---
+++ Gene_tree_topology/is_POT2_topo.py
@@ -0,0 +1,20 @@
+import sys
+import ete2
+from ete2 import Tree
+
+tree_file = sys.argv[1]
+Leaves = []
+for line in sys.stdin:
+    t = Tree(line[:-1])
+    for leaf in t:
+        Leaves.append(leaf.name)
+    for l in Leaves:
+        if l[:3] == "B71":
+            b71 = t&l
+            b_closest = min(Leaves, key=lambda x: b71.get_distance(x))
+            if b_closest[:3] == "GUY":
+                guy = t&b_closest
+                g_closest = min(Leaves, key=lambda x: guy.get_distance(x))
+                if g_closest == b71.name:
+                    print '\t'.join([tree_file, b71.name, guy.name])
+
