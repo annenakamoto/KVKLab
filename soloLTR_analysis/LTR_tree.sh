5a2c7cbcc512a0bc8f3e5f9fbfc81904f478823e
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-05 12:22:22 -0700

    LTR tree

--- soloLTR_analysis/LTR_tree.sh
+++ soloLTR_analysis/LTR_tree.sh
@@ -14,10 +14,15 @@ cd /global/scratch/users/annen/SOLO_LTRs
 
 LTR=$1
 
+> FASTA_FOR_TREE/${LTR}.fasta
 while read GENOME; do
     cat NEW_SOLOS/${LTR}.${GENOME}.solo.bed NEW_FLANK/${LTR}.${GENOME}.flank.bed > awk -v OFS='\t' -v genome=${GENOME} '{ print $1, $2, $3, genome "_" $4}' > BED_FOR_TREE/${LTR}.${GENOME}.all.bed
     bedtools getfasta -s -name -fo FASTA_FOR_TREE/${LTR}.${GENOME}.fasta -fi /global/scratch/users/annen/GENOME_TREE/hq_genomes/${GENOME}.fasta -bed BED_FOR_TREE/${LTR}.${GENOME}.all.bed
-    mafft --thread 24 FASTA_FOR_TREE/${LTR}.${GENOME}.fasta > ALIGNED_FOR_TREE/${LTR}.${GENOME}.afa
-    trimal -in ALIGNED_FOR_TREE/${LTR}.${GENOME}.afa -out ALIGNED_FOR_TREE/${LTR}.${GENOME}.trim.afa -noallgaps
-    raxmlHPC-PTHREADS-SSE3 -T 24 -n Raxml_${LTR}.${GENOME}.out -f a -x 12345 -p 12345 -# 100 -m GTRCAT -s ALIGNED_FOR_TREE/${LTR}.${GENOME}.trim.afa
+    cat FASTA_FOR_TREE/${LTR}.${GENOME}.fasta >> FASTA_FOR_TREE/${LTR}.fasta
 done < repgenome_list.txt
+
+mafft --thread 24 FASTA_FOR_TREE/${LTR}.fasta > ALIGNED_FOR_TREE/${LTR}.afa
+trimal -in ALIGNED_FOR_TREE/${LTR}.afa -out ALIGNED_FOR_TREE/${LTR}.trim.afa -noallgaps
+echo "*** aligned ${LTR} LTRs ***"
+raxmlHPC-PTHREADS-SSE3 -T 24 -n Raxml_${LTR}.out -f a -x 12345 -p 12345 -# 100 -m GTRCAT -s ALIGNED_FOR_TREE/${LTR}.trim.afa
+echo "*** made tree for ${LTR} LTRs ***"
