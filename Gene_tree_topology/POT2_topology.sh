f577a852d1a1e8b96421b532bb89545c0201ecf3
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-05 22:47:10 -0700

    pot2 topo for plotting

--- Gene_tree_topology/POT2_topology.sh
+++ Gene_tree_topology/POT2_topology.sh
@@ -13,13 +13,38 @@
 cd /global/scratch/users/annen/treeKO_analysis
 module unload python
 
-source activate /global/scratch/users/annen/anaconda3/envs/treeKO   # to use ete2 python module
+# source activate /global/scratch/users/annen/anaconda3/envs/treeKO   # to use ete2 python module
 
-> POT2_topo.DATA.txt
-ls ROOTED | while read TREE; do
-    cat ROOTED/${TREE} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/is_POT2_topo.py ${TREE} >> POT2_topo.DATA.txt
-    echo "*** finished ${TREE} ***"
+# make file containing the orthogroup + tree of genes that follow POT2 HT topology (based on closest distance within tree between B71 and guy11 genes)
+# > POT2_topo.DATA.txt
+# ls ROOTED | while read TREE; do
+#     cat ROOTED/${TREE} | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/is_POT2_topo.py ${TREE} >> POT2_topo.DATA.txt
+#     echo "*** finished ${TREE} ***"
+# done
+
+# conda deactivate
+# echo "*** DONE ***"
+
+# make file for plotting distance and permutation tests
+> POT2_topo.guy11.DATA.txt
+cat /global/scratch/users/annen/treeKO_analysis/gene_POT2.guy11.DATA.txt | while read line; do
+    match=$(grep $(echo ${line} | awk '{ print $4 }') POT2_topo.DATA.txt)
+    if [ -z "${match}" ]; then
+        # empty
+        echo ${line} | awk -v OFS='\t' '{ print $1, $2, $3, $4, $5, $6, "x" }' >> POT2_topo.guy11.DATA.txt
+    else
+        echo ${line} | awk -v OFS='\t' '{ print $1, $2, $3, $4, $5, $6, "POT2_topo" }' >> POT2_topo.guy11.DATA.txt
+    fi
 done
 
-conda deactivate
-echo "*** DONE ***"
+> POT2_topo.B71.DATA.txt
+cat /global/scratch/users/annen/treeKO_analysis/gene_POT2.B71.DATA.txt | while read line; do
+    match=$(grep $(echo ${line} | awk '{ print $4 }') POT2_topo.DATA.txt)
+    if [ -z "${match}" ]; then
+        # empty
+        echo ${line} | awk -v OFS='\t' '{ print $1, $2, $3, $4, $5, $6, "x" }' >> POT2_topo.B71.DATA.txt
+    else
+        echo ${line} | awk -v OFS='\t' '{ print $1, $2, $3, $4, $5, $6, "POT2_topo" }' >> POT2_topo.B71.DATA.txt
+    fi
+done
+
