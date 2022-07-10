019bdcca2aa2c53219111a32cc1c2f2ea684baa0
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-09 20:49:41 -0700

    ea18 b71 syri

--- PMB200B/syri_B71_EA18.sh
+++ PMB200B/syri_B71_EA18.sh
@@ -13,17 +13,27 @@
 cd /global/scratch/users/annen/SV_B71_EA18
 module load seqtk
 
-grep "chromosome" EA18.fasta | awk '{ print substr($1, 2)}' > EA18_chromnames.txt
-grep "chromosome" B71.fasta | awk '{ print substr($1, 2)}' > B71_chromnames.txt
+# grep "chromosome" EA18.fasta | awk '{ print substr($1, 2)}' > EA18_chromnames.txt
+# grep "chromosome" B71.fasta | awk '{ print substr($1, 2)}' > B71_chromnames.txt
 
-seqtk subseq EA18.fasta EA18_chromnames.txt | seqtk rename - chr > EA18_chrom_only.fasta
-seqtk subseq B71.fasta B71_chromnames.txt | seqtk rename - chr > B71_chrom_only.fasta
+# seqtk subseq EA18.fasta EA18_chromnames.txt | seqtk rename - chr > EA18_chrom_only.fasta
+# seqtk subseq B71.fasta B71_chromnames.txt | seqtk rename - chr > B71_chrom_only.fasta
 
 module purge
 module load mummer
 source activate /global/scratch/users/annen/anaconda3/envs/syri ## use source instead of conda
 
-nucmer --maxmatch -p EA18_v_B71_full -l 40 -g 90 -c 100 -b 200 -t 24 EA18_chrom_only.fasta B71_chrom_only.fasta
-delta-filter -m -i 90 -l 100 EA18_v_B71_full.delta > EA18_v_B71_full.filtered.delta
-show-coords -THrd EA18_v_B71_full.filtered.delta > EA18_v_B71_full.filtered.coords
-~/syri/syri/bin/syri -c EA18_v_B71_full.filtered.coords -d EA18_v_B71_full.filtered.delta -r MZ5-1-6_chrom_only.fasta -q B71_chrom_only.fasta --nc 5 --all
+# nucmer --maxmatch -p EA18_v_B71_full -l 40 -g 90 -c 100 -b 200 -t 24 EA18_chrom_only.fasta B71_chrom_only.fasta
+# delta-filter -m -i 90 -l 100 EA18_v_B71_full.delta > EA18_v_B71_full.filtered.delta
+# show-coords -THrd EA18_v_B71_full.filtered.delta > EA18_v_B71_full.filtered.coords
+~/syri/syri/bin/syri -c EA18_v_B71_full.filtered.coords -d EA18_v_B71_full.filtered.delta -r EA18_chrom_only.fasta -q B71_chrom_only.fasta --nc 5 --all
+
+conda deactivate
+source activate /global/scratch/users/annen/anaconda3/envs/plotsr
+
+echo -e $(realpath EA18_chrom_only.fasta)"\t"EA18 > genomes_list
+echo -e $(realpath B71_chrom_only.fasta)"\t"B71 >> genomes_list
+
+plotsr --sr syri.out --genomes genomes_list -o EA18_v_B71.png
+
+conda deactivate
