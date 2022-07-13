40a8943ec722c4240673b994e9731d470129c655
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-13 08:31:35 -0700

    art max pav

--- Expanded_effectors/ART_MAX_pav.sh
+++ Expanded_effectors/ART_MAX_pav.sh
@@ -13,12 +13,14 @@
 cd /global/scratch/users/annen/Expanded_effectors
 
 > ART_MAX_pav.DATA.txt
+echo -e "Effector\tOG\t70-15\tguy11\tAV1-1-1\tFJ72ZC7-77\tFJ81278\tFJ98099\tFR13\tSar-2-20-1\tUS71\tB71\tBR32\tLpKY97\tMZ5-1-6\tCD156\tNI907" >> ART_MAX_pav.DATA.txt
 > ART_MAX_pav.copynum.DATA.txt
+echo -e "Effector\tOG\t70-15\tguy11\tAV1-1-1\tFJ72ZC7-77\tFJ81278\tFJ98099\tFR13\tSar-2-20-1\tUS71\tB71\tBR32\tLpKY97\tMZ5-1-6\tCD156\tNI907" >> ART_MAX_pav.copynum.DATA.txt
 while read g; do
     MGG=$(echo ${g} | awk '{ print $1 }')
     GROUP=$(echo ${g} | awk '{ print $2 }')
     ### columns: Group, OG, 70-15_gene, guy11_gene, etc.
-    grep ${MGG} /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups.txt | python /global/scratch/users/annen/KVKLab/Expanded_effectors/pav_line.py ${GROUP} "names" > ART_MAX_pav.DATA.txt
-    grep ${MGG} /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups.txt | python /global/scratch/users/annen/KVKLab/Expanded_effectors/pav_line.py ${GROUP} "num" > ART_MAX_pav.copynum.DATA.txt
+    grep ${MGG} /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups.txt | python /global/scratch/users/annen/KVKLab/Expanded_effectors/pav_line.py ${GROUP} "names" >> ART_MAX_pav.DATA.txt
+    grep ${MGG} /global/scratch/users/annen/GENOME_TREE/OrthoFinder_out/Results_Jun21/Orthogroups/Orthogroups.txt | python /global/scratch/users/annen/KVKLab/Expanded_effectors/pav_line.py ${GROUP} "num" >> ART_MAX_pav.copynum.DATA.txt
 done < ART_MAX_list.txt
 
