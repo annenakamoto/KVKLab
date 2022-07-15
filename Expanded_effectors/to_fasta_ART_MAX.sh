73a64e9b9de9cdaa3d264c29db7f5e279730cdbe
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-14 17:35:19 -0700

    art max fasta strand

--- Expanded_effectors/to_fasta_ART_MAX.sh
+++ Expanded_effectors/to_fasta_ART_MAX.sh
@@ -16,12 +16,12 @@ while read GENOME; do
     ### make ART fasta
     echo "*** making ART fasta for ${GENOME} ***"
     cat GENE_BED/info_genes_${GENOME}.bed | awk -v OFS='\t' '/gene/&&/ART/ { print $1, $2, $3, $4 ":" $8; }' > tmp.${GENOME}.ART.bed
-    bedtools getfasta -name -fi /global/scratch/users/annen/GENOME_TREE/hq_genomes/${GENOME}.fasta -fo FASTA/${GENOME}.ART.fasta -bed tmp.${GENOME}.ART.bed
+    bedtools getfasta -name -s -fi /global/scratch/users/annen/GENOME_TREE/hq_genomes/${GENOME}.fasta -fo FASTA/${GENOME}.ART.fasta -bed tmp.${GENOME}.ART.bed
     rm tmp.${GENOME}.ART.bed
     ### make MAX fasta
     echo "*** making MAX fasta for ${GENOME} ***"
     cat GENE_BED/info_genes_${GENOME}.bed | awk -v OFS='\t' '(/MAX/||/AVR/||/TOX/)&&/gene/ { print $1, $2, $3, $4 ":" $8; }' > tmp.${GENOME}.MAX.bed
-    bedtools getfasta -name -fi /global/scratch/users/annen/GENOME_TREE/hq_genomes/${GENOME}.fasta -fo FASTA/${GENOME}.MAX.fasta -bed tmp.${GENOME}.MAX.bed
+    bedtools getfasta -name -s -fi /global/scratch/users/annen/GENOME_TREE/hq_genomes/${GENOME}.fasta -fo FASTA/${GENOME}.MAX.fasta -bed tmp.${GENOME}.MAX.bed
     rm tmp.${GENOME}.MAX.bed
 done < genome_list.txt
 
