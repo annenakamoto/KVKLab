81819634b79fac7861ced3634d2fee8ced163e45
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-14 18:05:09 -0700

    ART MAX trees

--- Expanded_effectors/allblast_ART_MAX.sh
+++ Expanded_effectors/allblast_ART_MAX.sh
@@ -12,12 +12,12 @@
 
 cd /global/scratch/users/annen/Expanded_effectors
 
-> FASTA/ART.fasta
-> FASTA/MAX.fasta
-while read GENOME; do
-    cat FASTA/${GENOME}.ART.fasta >> FASTA/ART.fasta
-    cat FASTA/${GENOME}.MAX.fasta >> FASTA/MAX.fasta
-done < genome_list.txt
+# > FASTA/ART.fasta
+# > FASTA/MAX.fasta
+# while read GENOME; do
+#     cat FASTA/${GENOME}.ART.fasta >> FASTA/ART.fasta
+#     cat FASTA/${GENOME}.MAX.fasta >> FASTA/MAX.fasta
+# done < genome_list.txt
 
 # makeblastdb -in FASTA/ART.fasta -dbtype nucl -out ART_db
 # blastn -db ART_db -query FASTA/ART.fasta -outfmt 6 -out allblast_ART.out -num_threads 24
@@ -30,8 +30,14 @@ done < genome_list.txt
 # mafft --maxiterate 1000 --globalpair --quiet --thread 24 FASTA/MAX.fasta > FASTA/MAX_align.fasta
 # echo "*** done ***"
 
-trimal -noallgaps -in FASTA/ART_align.fasta -out FASTA/ART_align.trim.fasta
-trimal -noallgaps -in FASTA/MAX_align.fasta -out FASTA/MAX_align.trim.fasta
+### doesn't remove enough gaps
+# trimal -noallgaps -in FASTA/ART_align.fasta -out FASTA/ART_align.trim.fasta
+# trimal -noallgaps -in FASTA/MAX_align.fasta -out FASTA/MAX_align.trim.fasta
 
-trimal -nogaps -in FASTA/ART_align.fasta -out FASTA/ART_align.trim.nogap.fasta
-trimal -nogaps -in FASTA/MAX_align.fasta -out FASTA/MAX_align.trim.nogap.fasta
+### this trimming method makes the alignment look a lot better
+trimal -gappyout -in FASTA/ART_align.fasta -out FASTA/ART_align.trim.gappy.fasta
+trimal -gappyout -in FASTA/MAX_align.fasta -out FASTA/MAX_align.trim.gappy.fasta
+
+### make ART and MAX trees
+raxmlHPC-PTHREADS-SSE3 -s FASTA/ART_align.trim.gappy.fasta -n RAxML.ART -m GTRGAMMA -T 24 -f a -x 12345 -p 12345 -# 100
+raxmlHPC-PTHREADS-SSE3 -s FASTA/MAX_align.trim.gappy.fasta -n RAxML.MAX -m GTRGAMMA -T 24 -f a -x 12345 -p 12345 -# 100
