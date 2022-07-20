323cefbfda1321a63be4cc601a1e788902ee897f
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-20 14:00:06 -0700

    NI907 cds

--- Gene_tree_topology/NI907_prot_to_nuc.py
+++ Gene_tree_topology/NI907_prot_to_nuc.py
@@ -4,7 +4,7 @@ MAPPING = {}    # key = old gene name, value = new gene name
 with open("/global/scratch/users/annen/GENOME_TREE/PROTEOMES/NI907_gene_map.txt", "r") as map:
     for line in map:
         lst = line.split()
-        MAPPING[lst[1]] = lst[2]
+        MAPPING[lst[0]] = lst[1]
 
 # sys.stdin is /global/scratch/users/annen/GENOME_TREE/PROTEOMES/GCF_004355905.1_ASM435590v1_cds_from_genomic.fna
 for line in sys.stdin:
