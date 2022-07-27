089b613f8387339a0805354135d0f5fdd8463873
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-07-27 08:54:05 -0700

    pos_GC_JC_LTR

--- Jukes-Cantor/visualize_TEs.sh
+++ Jukes-Cantor/visualize_TEs.sh
@@ -10,7 +10,7 @@
 
 ### Generate bed files to visualize TE position and JC distance
 
-cd /global/scratch/users/annen/visualize_TEs
+#cd /global/scratch/users/annen/visualize_TEs
 
 # while read TE; do
 #     while read genome; do
@@ -54,19 +54,31 @@ cd /global/scratch/users/annen/visualize_TEs
 ###     LTR_DIV_RESULTS/ALL_RESULTS.LTRdiv.txt contains columns: LTR(name)	pair(#)	genome	divergence
 ###     MAPPING/${TE}.${GENOME}_mapping.txt contains columns:   pair(#): chrom  start   end LTR(name)   score(0)    strand
 ###         EXAMPLE:    1: MQOP01000001.1	2161562	2169220	MAGGY_I	0	+
-cd /global/scratch/users/annen/LTR_divergence
+# cd /global/scratch/users/annen/LTR_divergence
+# while read TE; do
+#     > itol_LTR_ds.${TE}.txt
+#     echo "DATASET_GRADIENT" >> itol_LTR_ds.${TE}.txt
+#     echo "SEPARATOR SPACE" >> itol_LTR_ds.${TE}.txt
+#     echo "DATASET_LABEL LTR" >> itol_LTR_ds.${TE}.txt
+#     echo "COLOR #ff0000" >> itol_LTR_ds.${TE}.txt
+#     echo "COLOR_MIN #ff0000" >> itol_LTR_ds.${TE}.txt
+#     echo "COLOR_MAX #0000ff" >> itol_LTR_ds.${TE}.txt
+#     echo "DATA" >> itol_LTR_ds.${TE}.txt
+#     TE_short=$(echo ${TE} | awk -v FS="_" '{ print $1 }')
+#     while read genome; do
+#         cat LTR_DIV_RESULTS/ALL_RESULTS.LTRdiv.txt | awk -v g=${genome} -v t=${TE_short} '$1 ~ t && $3 ~ g' | python /global/scratch/users/annen/KVKLab/Jukes-Cantor/itol_LTR_ds.py MAPPING/${TE}.${genome}_mapping.txt ${genome} >> itol_LTR_ds.${TE}.txt
+#     done < genome_list.txt
+# done < TE_list.txt
+
+### produce a file with columns: chr   start   end   TE_name   genome   GC_content   JC_one_cons   JC_lin_cons   LTR_div
+###     1: TE
+###     2: RIP_analysis/name_gc_${TE}.txt
+###     3: visualize_TEs/itol_JC_ds.${TE}.txt
+###     4: JC_dist_indiv_TEs/itol_JC_ds_lin.${TE}.txt
+###     5: LTR_divergence/itol_LTR_ds.${TE}.txt
+cd /global/scratch/users/annen
+> pos_GC_JC_LTR.DATA.txt
 while read TE; do
-    > itol_LTR_ds.${TE}.txt
-    echo "DATASET_GRADIENT" >> itol_LTR_ds.${TE}.txt
-    echo "SEPARATOR SPACE" >> itol_LTR_ds.${TE}.txt
-    echo "DATASET_LABEL LTR" >> itol_LTR_ds.${TE}.txt
-    echo "COLOR #ff0000" >> itol_LTR_ds.${TE}.txt
-    echo "COLOR_MIN #ff0000" >> itol_LTR_ds.${TE}.txt
-    echo "COLOR_MAX #0000ff" >> itol_LTR_ds.${TE}.txt
-    echo "DATA" >> itol_LTR_ds.${TE}.txt
-    TE_short=$(echo ${TE} | awk -v FS="_" '{ print $1 }')
-    while read genome; do
-        cat LTR_DIV_RESULTS/ALL_RESULTS.LTRdiv.txt | awk -v g=${genome} -v t=${TE_short} '$1 ~ t && $3 ~ g' | python /global/scratch/users/annen/KVKLab/Jukes-Cantor/itol_LTR_ds.py MAPPING/${TE}.${genome}_mapping.txt ${genome} >> itol_LTR_ds.${TE}.txt
-    done < genome_list.txt
-done < TE_list.txt
+    python KVKLab/Jukes-Cantor/pos_GC_JC_LTR.py ${TE} RIP_analysis/name_gc_${TE}.txt visualize_TEs/itol_JC_ds.${TE}.txt JC_dist_indiv_TEs/itol_JC_ds_lin.${TE}.txt LTR_divergence/itol_LTR_ds.${TE}.txt >> pos_GC_JC_LTR.DATA.txt
+done < LTR_divergence/TE_list.txt
 
