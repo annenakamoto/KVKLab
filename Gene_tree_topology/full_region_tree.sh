cb94d5778033c39c26936bf90ad77104d402c47d
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-08-03 10:37:26 -0700

    full pot2 topo region tree

--- Gene_tree_topology/full_region_tree.sh
+++ Gene_tree_topology/full_region_tree.sh
@@ -17,6 +17,7 @@ cd /global/scratch/users/annen
 # echo -e "CM015706.1\t277197\t316758\tsec2_B71\t0\t+" > POT2_topo_region/sec2_B71.bed
 # echo -e "MQOP01000008.1\t203887\t860573\tsec1_guy11\t0\t+" > POT2_topo_region/sec1_guy11.bed
 # echo -e "MQOP01000016.1\t316973\t356717\tsec2_guy11\t0\t-" > POT2_topo_region/sec2_guy11.bed
+echo -e "MQOP01000016.1\t316973\t356717\tsec2_guy11\t0\t+" > POT2_topo_region/sec2_guy11.bed
 # echo -e "UCNY03000007.1\t1454502\t2050941\tsec1_US71\t0\t-" > POT2_topo_region/sec1_US71.bed
 # echo -e "UCNY03000016.1\t258867\t298214\tsec2_US71\t0\t+" > POT2_topo_region/sec2_US71.bed
 # echo -e "CP050926.1\t642233\t1219575\tsec1_LpKY97\t0\t+" > POT2_topo_region/sec1_LpKY97.bed
@@ -27,12 +28,12 @@ cd /global/scratch/users/annen
 # echo -e "CM015044.1\t95441\t146289\tsec2_NI907\t0\t-" > POT2_topo_region/sec2_NI907.bed
 
 ### get fasta for each section
-# > POT2_topo_region/sec1.fasta
-# > POT2_topo_region/sec2.fasta
-# while read GENOME; do
-#     bedtools getfasta -name -s -fi GENOME_TREE/hq_genomes/${GENOME}.fasta -bed POT2_topo_region/sec1_${GENOME}.bed >> POT2_topo_region/sec1.fasta
-#     bedtools getfasta -name -s -fi GENOME_TREE/hq_genomes/${GENOME}.fasta -bed POT2_topo_region/sec2_${GENOME}.bed >> POT2_topo_region/sec2.fasta
-# done < rep_genomes_list.txt
+> POT2_topo_region/sec1.fasta
+> POT2_topo_region/sec2.fasta
+while read GENOME; do
+    #bedtools getfasta -name -s -fi GENOME_TREE/hq_genomes/${GENOME}.fasta -bed POT2_topo_region/sec1_${GENOME}.bed >> POT2_topo_region/sec1.fasta
+    bedtools getfasta -name -s -fi GENOME_TREE/hq_genomes/${GENOME}.fasta -bed POT2_topo_region/sec2_${GENOME}.bed >> POT2_topo_region/sec2.fasta
+done < rep_genomes_list.txt
 
 ### align, trim, make tree
 cd POT2_topo_region
@@ -40,15 +41,15 @@ cd POT2_topo_region
 # mafft --maxiterate 1000 --globalpair --quiet --thread 24 sec2.fasta > sec2.afa
 #echo "*** starting sec1 alignment..."
 #mafft --auto --quiet --thread 24 sec1.fasta > sec1.afa
-#echo "*** done, starting sec2 alignment..."
-#mafft --auto --quiet --thread 24 sec2.fasta > sec2.afa
+echo "*** done, starting sec2 alignment..."
+mafft --auto --quiet --thread 24 sec2.fasta > sec2.afa
 echo "*** done, trimming..."
-trimal -gappyout -in sec1.afa -out sec1.trim1.afa
+#trimal -gappyout -in sec1.afa -out sec1.trim1.afa
 trimal -gappyout -in sec2.afa -out sec2.trim1.afa
 echo "*** done, making trees..."
-cat sec1.trim1.afa | tr \( \{ | tr \) \} > sec1.trim.afa
+#cat sec1.trim1.afa | tr \( \{ | tr \) \} > sec1.trim.afa
 cat sec2.trim1.afa | tr \( \{ | tr \) \} > sec2.trim.afa
-raxmlHPC-PTHREADS-SSE3 -s sec1.trim.afa -n RAxML.sec1 -m GTRGAMMA -T 20 -f a -x 12345 -p 12345 -# 100
+#raxmlHPC-PTHREADS-SSE3 -s sec1.trim.afa -n RAxML.sec1 -m GTRGAMMA -T 20 -f a -x 12345 -p 12345 -# 100
 raxmlHPC-PTHREADS-SSE3 -s sec2.trim.afa -n RAxML.sec2 -m GTRGAMMA -T 20 -f a -x 12345 -p 12345 -# 100
 echo "*** done"
 
