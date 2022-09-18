4d55ab9e0ae83cc0951707347be75f2508832b66
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-09-18 10:52:51 -0700

    common go terms

--- Gene_tree_topology/common_GO_terms.py
+++ Gene_tree_topology/common_GO_terms.py
@@ -11,27 +11,27 @@ for line in sys.stdin:
     desc = lst[3]
     if "gene" in gene_name:
         if ont == "BP":
-            if not BP.get(list(goid, desc)):
-                BP[list(goid, desc)] = 1
+            if not BP.get(goid + "\t" + desc):
+                BP[goid + "\t" + desc] = 1
             else:
-                BP[list(goid, desc)] += 1
+                BP[goid + "\t" + desc] += 1
         if ont == "MF":
-            if not MF.get(list(goid, desc)):
-                MF[list(goid, desc)] = 1
+            if not MF.get(goid + "\t" + desc):
+                MF[goid + "\t" + desc] = 1
             else:
-                MF[list(goid, desc)] += 1
+                MF[goid + "\t" + desc] += 1
         if ont == "CC":
-            if not CC.get(list(goid, desc)):
-                CC[list(goid, desc)] = 1
+            if not CC.get(goid + "\t" + desc):
+                CC[goid + "\t" + desc] = 1
             else:
-                CC[list(goid, desc)] += 1
+                CC[goid + "\t" + desc] += 1
 
 print("*** BP ***")
 for k,v in sorted(BP.items(), key=lambda kv: kv[1], reverse=True):
-    print(v + "\t" + k[0] + "\t" + k[1])
+    print(v + "\t" + k)
 print("*** MF ***")
 for k,v in sorted(MF.items(), key=lambda kv: kv[1], reverse=True):
-    print(v + "\t" + k[0] + "\t" + k[1])
+    print(v + "\t" + k)
 print("*** CC ***")
 for k,v in sorted(CC.items(), key=lambda kv: kv[1], reverse=True):
-    print(v + "\t" + k[0] + "\t" + k[1])
+    print(v + "\t" + k)
