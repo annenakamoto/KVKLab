a3afef1eec7021eb5bc5e83fc12a1ab3660445a5
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2022-09-18 12:22:43 -0700

    pfam terms

--- Gene_tree_topology/GO_PFAM_pot2topo.sh
+++ Gene_tree_topology/GO_PFAM_pot2topo.sh
@@ -19,9 +19,16 @@ while read GENOME; do
     ### python script to construct GO term table
     ###     columns: gene_name  MF_goid BP_goid CC_goid     MF_desc BP_desc CC_desc
     #cat ${GENOME}.GO.filt.out | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/GO_table.py ${GENOME} > ${GENOME}.GO.TABLE.txt
-    cat ${GENOME}.GO.filt.out | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/common_GO_terms.py > ${GENOME}.common_GO_terms.txt
+    #cat ${GENOME}.GO.filt.out | python /global/scratch/users/annen/KVKLab/Gene_tree_topology/common_GO_terms.py > ${GENOME}.common_GO_terms.txt
 
 done < genome_list.txt
 
-### PFAM term analysis
 
+### PFAM term analysis
+source activate /global/scratch/users/annen/anaconda3/envs/pfam_scan.pl
+while read GENOME; do
+    ### find pfam domains in genes in POT2 topo region
+    pfam_scan.pl -fasta sec1_${GENOME}_genes.faa -dir PFAM_lib -e_dom 0.01 -e_seq 0.01 -outfile sec1_${GENOME}_genes.pfam.out
+    ### parse the output
+    cat sec1_${GENOME}_genes.pfam.out | python /global/scratch/users/annen/KVKLab/Phase1/parse_pfam.py > sec1_${GENOME}.pfam_list.out
+done < genome_list.txt
