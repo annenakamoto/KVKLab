da296ebb7d0aa3ec6bedb57faabfb7c9cdff7d4b
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-10 13:28:03 -0800

    Zm mmseqs

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/mmseqs_Zm.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/mmseqs_Zm.sh
@@ -21,14 +21,14 @@ cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
 # cat maize_NAM_proteomes/*protein.fa | awk -v RS=">" '/P001/ { print ">" substr($0, 1, length($0)-1); }' > Zm_panPROTEOME.fa
 
 ### Run MMSeqs2 on the filtered pan-proteome
-conda activate MMseqs2
+source activate /global/scratch/users/annen/anaconda3/envs/MMseqs2
 echo "*** creating mmseqs database ***"
 mmseqs createdb Zm_panPROTEOME.fa Zm_panPROTEOME                                            # convert fasta file to MMseqs2 database format
 echo "*** running mmseqs linclust ***"
 mmseqs linclust Zm_panPROTEOME Zm_panPROTEOME_clu tmp --cov-mode 0 -c 0.5                   # run the linear clustering algorithm on the database
 echo "*** producing msa to center sequence ***"
 mmseqs result2msa Zm_panPROTEOME Zm_panPROTEOME Zm_panPROTEOME_clu Zm_panPROTEOME_clu_msa   # produce an MSA to center sequence (pseudo alignment)
-conda deactivate
+source deactivate
 
 ### download Maize_NLRome_GeneTable.txt
 # wget https://github.com/daniilprigozhin/NLRCladeFinder/raw/main/Maize_NLRome/Maize_NLRome_GeneTable.txt
