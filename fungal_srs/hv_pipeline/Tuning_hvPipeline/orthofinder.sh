267f86d3ea1bdebd9135e8e49282be51cf50c865
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-16 15:08:23 -0700

    orthofinder

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/orthofinder.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/orthofinder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=mmseqs_Zm
+#SBATCH --job-name=orthofinder
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
@@ -7,10 +7,12 @@
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=ALL
 
+cd /global/scratch/users/annen
+wget https://zenodo.org/record/7651486/files/analysis_files.tar.gz?download=1
+
 cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
 
 rm -r OrthoFinder_out
-
 source activate /global/scratch/users/annen/anaconda3/envs/OrthoFinder
 orthofinder -oa -f OrthoFinder_in -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -o OrthoFinder_out
 conda deactivate
