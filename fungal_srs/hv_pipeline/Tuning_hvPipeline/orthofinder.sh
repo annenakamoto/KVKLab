581d7d03958096c159acd1196dc007f684109b19
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-16 21:38:33 -0700

    orthofinder

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/orthofinder.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/orthofinder.sh
@@ -7,9 +7,16 @@
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=ALL
 
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/maize_NAM_proteomes
+
+# ls *.protein.fa | while read fa; do
+#     cat ${fa} | awk -v RS=">" '/P001/ { print ">" substr($0, 1, length($0)-1); }' > /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OrthoFinder_in/${fa}
+# done
+
 cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
+module purge
 
 rm -r OrthoFinder_out
 source activate /global/scratch/users/annen/anaconda3/envs/OrthoFinder
-orthofinder -oa -f OrthoFinder_in -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -o OrthoFinder_out
+orthofinder -oa -f OrthoFinder_in -t 24 -a 5 -M msa -S diamond_ultra_sens -A mafft -T fasttree -X -o OrthoFinder_out
 conda deactivate
