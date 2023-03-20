0402a75c6eabd30cce6ab08754069d34a522b5a8
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-20 11:47:17 -0700

    parse pfam out

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/common_dom.py
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/common_dom.py
@@ -6,17 +6,19 @@ for line in sys.stdin:
     arch = lst[4].split("|")
     set_all = lst[5].split("|")
     for d in arch:
-        if not DOMAINS.get(d):
-            DOMAINS[d] = [1, 0]
-        else:
-            DOMAINS[d][0] += 1
+        if d != "None":
+            if not DOMAINS.get(d):
+                DOMAINS[d] = [1, 0]
+            else:
+                DOMAINS[d][0] += 1
     for d in set_all:
-        n = len(d.split(",")[2]) + 1
-        dom = d[:-n]
-        if not DOMAINS.get(dom):
-            DOMAINS[dom] = [0, 1]
-        else:
-            DOMAINS[dom][1] += 1
+        if d != "None":
+            n = len(d.split(",")[2]) + 1
+            dom = d[:-n]
+            if not DOMAINS.get(dom):
+                DOMAINS[dom] = [0, 1]
+            else:
+                DOMAINS[dom][1] += 1
 
 for k,v in sorted(DOMAINS.items(), key=lambda x:x[1][0], reverse=True):
     print("\t".join([k, str(v[0]), str(v[1])]))
