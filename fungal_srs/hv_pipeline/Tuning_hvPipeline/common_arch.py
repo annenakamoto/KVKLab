a975bc36112648f644683e0773be1041b2e69701
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-20 11:29:21 -0700

    parse pfam out

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/common_arch.py
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/common_arch.py
@@ -1,3 +1,14 @@
 import sys
 
+DOM_ARCH = {}   # key=domain_architecture, value=count
+for line in sys.stdin:
+    lst = line.split()
+    og = lst[0]
+    arch = lst[4]
+    if DOM_ARCH.get(arch):
+        DOM_ARCH[arch].append(og)
+    else:
+        DOM_ARCH[arch] = [og]
 
+for k,v in sorted(DOM_ARCH.items(), key=lambda x:len(x[1]), reverse=True):
+    print(k + "\t" + str(len(v)) + "\t" + ",".join(v))
--- fungal_srs/hv_pipeline/Tuning_hvPipeline/common_dom.py
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/common_dom.py
@@ -1,3 +1,22 @@
 import sys
 
-###
+DOMAINS = {}    # key=domain, value=[count_arch, count_all]
+for line in sys.stdin:
+    lst = line.split()
+    arch = lst[4].split("|")
+    set_all = lst[5].split("|")
+    for d in arch:
+        if not DOMAINS.get(d):
+            DOMAINS[d] = [1, 0]
+        else:
+            DOMAINS[d][0] += 1
+    for d in set_all:
+        n = len(d.split(",")[2]) + 1
+        dom = d[:-n]
+        if not DOMAINS.get(dom):
+            DOMAINS[dom] = [0, 1]
+        else:
+            DOMAINS[dom][1] += 1
+
+for k,v in sorted(DOMAINS.items(), key=lambda x:x[1][0], reverse=True):
+    print("\t".join([k, str(v[0]), str(v[1])]))
