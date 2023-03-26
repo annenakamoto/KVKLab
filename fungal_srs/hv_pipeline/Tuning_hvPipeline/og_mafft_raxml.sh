128b6b863a6695b0b774142601138c0cd8128e93
Anne Nakamoto <annenakamoto@ras-31-155.vpn.berkeley.edu>
2023-03-25 19:15:20 -0700

    og mafft

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
@@ -7,13 +7,13 @@
 #SBATCH --mail-user=annen@berkeley.edu
 #SBATCH --mail-type=FAIL
 
-OG=${1}     # the orthogroup is taken as input
-
 cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
 
 ### run mafft on OG
-mafft --maxiterate 1000 --localpair --thread 24 OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/${OG}.fa > OG_MAFFT/${OG}.afa
+ls OrthoFinder_out/Results_Mar16/MultipleSequenceAlignments/ | awk '/OG/ { print substr($1,1,9); }' | while read OG; do
+    mafft --maxiterate 1000 --localpair --thread 24 OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/${OG}.fa > OG_MAFFT/${OG}.afa
+done
 
 ### run raxml on OG alignment
-cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_RAXML
-raxmlHPC-PTHREADS-SSE3 -s /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_MAFFT/${OG}.afa -n RAxML.${OG} -T 24 -m PROTCATJTT -f a -x 12345 -p 12345 -# 100
+# cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_RAXML
+# raxmlHPC-PTHREADS-SSE3 -s /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_MAFFT/${OG}.afa -n RAxML.${OG} -T 24 -m PROTCATJTT -f a -x 12345 -p 12345 -# 100
