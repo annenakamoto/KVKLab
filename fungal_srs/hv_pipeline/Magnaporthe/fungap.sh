69a7c90138d92fb1a62ff26bdc5425c2cd6a74ee
Anne Nakamoto <annenakamoto@ras-31-155.vpn.berkeley.edu>
2023-03-25 21:58:27 -0700

    fungap

---
+++ fungal_srs/hv_pipeline/Magnaporthe/fungap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=fungap
+#SBATCH --partition=savio
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=/global/scratch/users/annen/stdout_slurm/slurm-%j.out
+#SBATCH --error=/global/scratch/users/annen/stderr_slurm/slurm-%j.out
+
+GCA=${1}
+SRA=${2}
+
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoFunGAP
+mkdir ${GCA}
+cd ${GCA}
+mkdir fungap_out
+
+module purge
+export PERL5LIB=''
+## this path has to be set otherwise augustus tries to write within the singularity container
+export AUGUSTUS_CONFIG_PATH=/global/scratch/users/annen/fungap_tarball/fungap_config_copy/
+
+## for GeneMark to work, a non-expired gm_key_64 has to be placed at ~/.gm_key 
+
+assembly=$(ls ../../MoGENOMES_all/${GCA}*)
+
+singularity exec /global/scratch/users/annen/fungap_tarball/fungap.sif python /workspace/FunGAP/fungap.py \
+        --output_dir fungap_out \
+        --trans_read_1 ../run_files/${SRA}_1.fastq \
+        --trans_read_2 ../run_files/${SRA}_2.fastq \
+        --genome_assembly ${assembly}  \
+        --augustus_species magnaporthe_grisea  \
+        --sister_proteome ../run_files/prot_db.faa  \
+        --busco_dataset sordariomycetes_odb10 \
+        --num_cores ${SLURM_NTASKS}
---
+++ fungal_srs/hv_pipeline/Magnaporthe/prep_fungap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=prep_fungap
+#SBATCH --partition=savio2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoFunGAP/run_files
+
+### download all RNA-Seq datasets
+modile load sra-tools
+
+> sra_list.txt
+echo "SRR8842990" >> sra_list.txt   # MoO (guy11) that I used
+echo "ERR5875670" >> sra_list.txt   # MoO (guy11) that Pierre used
+echo "SRR9126640" >> sra_list.txt   # MoT (B71)
+echo "SRR8278105" >> sra_list.txt   # MoE (MZ5-1-6)
+
+while read sra; do
+    echo ${sra}
+    fastq-dump -I --split-files ${sra}
+    wc -l ${sra}_[12].fastq
+done < sra_list.txt
+
+rm sra_list.txt
