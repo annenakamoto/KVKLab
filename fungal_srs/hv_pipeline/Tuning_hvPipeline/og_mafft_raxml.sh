3aad1543bf045dde2d2f23470ece7b3ab053c9a5
Anne Nakamoto <annenakamoto@ras-31-155.vpn.berkeley.edu>
2023-03-25 19:16:07 -0700

    og mafft

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
@@ -12,6 +12,7 @@ cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
 ### run mafft on OG
 ls OrthoFinder_out/Results_Mar16/MultipleSequenceAlignments/ | awk '/OG/ { print substr($1,1,9); }' | while read OG; do
     mafft --maxiterate 1000 --localpair --thread 24 OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/${OG}.fa > OG_MAFFT/${OG}.afa
+    echo "${OG} done"
 done
 
 ### run raxml on OG alignment
