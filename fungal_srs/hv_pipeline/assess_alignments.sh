6851b1cce53c7d815c16ea39d19136e7aeb10564
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-27 18:00:10 -0700

    assess alignment hv

---
+++ fungal_srs/hv_pipeline/assess_alignments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=R_assess_alignments
+#SBATCH --partition=savio2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+cd /global/scratch/users/annen
+
+source activate /global/scratch/users/annen/anaconda3/envs/R
+
+### run the R script that outputs a list of OGs with alignments that pass hv parameters
+Rscript KVKLab/fungal_srs/hv_pipeline/assess_alignment.R | awk '/[1]/ { print substr($2,2,9); }' > HValn_OGs_list.txt
+
+conda deactivate
