de0b020e82877d5277d7a09d257220345836fac0
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-03-28 03:57:11 -0700

    raxml for nlrs

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=og_mafft_raxml
+#SBATCH --job-name=og_raxml
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
@@ -16,11 +16,12 @@ cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
 # done
 
 ### ran out of memory after OG0028794, run what's left (starting with OG0028795)
-cat ../../remaining_OG.txt | while read OG; do
-    mafft --maxiterate 1000 --localpair --thread 24 --quiet OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/${OG}.fa > OG_MAFFT/${OG}.afa
-    echo "${OG} done"
-done
+# cat ../../remaining_OG.txt | while read OG; do
+#     mafft --maxiterate 1000 --localpair --thread 24 --quiet OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/${OG}.fa > OG_MAFFT/${OG}.afa
+#     echo "${OG} done"
+# done
 
+OG=${1}
 ### run raxml on OG alignment
-# cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_RAXML
-# raxmlHPC-PTHREADS-SSE3 -s /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_MAFFT/${OG}.afa -n RAxML.${OG} -T 24 -m PROTCATJTT -f a -x 12345 -p 12345 -# 100
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_RAXML
+raxmlHPC-PTHREADS-SSE3 -s /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/OG_MAFFT/${OG}.afa -n RAxML.${OG} -T 24 -m PROTCATJTT -f a -x 12345 -p 12345 -# 100
