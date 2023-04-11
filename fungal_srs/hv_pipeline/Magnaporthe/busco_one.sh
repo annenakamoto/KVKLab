060ee16fa1710bf0146ec0ea157fbce149a97101
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-04-10 17:00:07 -0700

    busco one genome

--- fungal_srs/hv_pipeline/Magnaporthe/busco_one.sh
+++ fungal_srs/hv_pipeline/Magnaporthe/busco_one.sh
@@ -10,11 +10,10 @@
 cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder/MoBUSCO
 
 genome=${1}
-mkdir -p ${genome}
-cd ${genome}
+
 ### run busco on a genome
 module purge    # loaded modules interfere with busco
 source activate /global/scratch/users/annen/anaconda3/envs/busco
-busco -i ../../MoASSEMBLIES/${genome}.fna -l sordariomycetes_odb10 -m genome -c ${SLURM_NTASKS} -f
+busco -i ../MoASSEMBLIES/${genome}.fna -o ${genome} -l sordariomycetes_odb10 -m genome -c ${SLURM_NTASKS} -f
 conda deactivate
 
