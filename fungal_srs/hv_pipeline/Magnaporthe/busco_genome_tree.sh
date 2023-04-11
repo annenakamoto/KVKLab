933d61f85f3d9484f7262efdf93206d22ca4060c
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-04-11 15:47:54 -0700

    busco phylo

--- fungal_srs/hv_pipeline/Magnaporthe/busco_genome_tree.sh
+++ fungal_srs/hv_pipeline/Magnaporthe/busco_genome_tree.sh
@@ -11,10 +11,16 @@
 
 cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder
 
-module purge
-echo "activating conda env..."
-source activate /global/scratch/users/annen/anaconda3/envs/BUSCO_phylogenomics
-echo "env activated"
-python ../../BUSCO_phylogenomics/BUSCO_phylogenomics.py -i MoBUSCO -o MoBUSCO_PHYLO -t ${SLURM_NTASKS} --supermatrix_only --gene_tree_program fasttree > BUSCO_phylogenomics.LOG.txt
-conda deactivate
+# module purge
+# echo "activating conda env..."
+# source activate /global/scratch/users/annen/anaconda3/envs/BUSCO_phylogenomics
+# echo "env activated"
+# python ../../BUSCO_phylogenomics/BUSCO_phylogenomics.py -i MoBUSCO -o MoBUSCO_PHYLO -t ${SLURM_NTASKS} --supermatrix_only --gene_tree_program fasttree > BUSCO_phylogenomics.LOG.txt
+# conda deactivate
+
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder/MoBUSCO_PHYLO/supermatrix/proteins
+ls ${1} | while read faa; do
+    mafft --maxiterate 1000 --globalpair --thread ${SLURM_NTASKS} ${faa} > ../../../MoBUSCO_MAFFT/${faa}
+done
+
 
