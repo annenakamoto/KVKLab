102935e2725c24c82b8f6e09b0ddb3a8aa495692
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-04-16 19:47:03 -0700

    pfam domain arch

--- fungal_srs/hv_pipeline/Magnaporthe/dom_arch.sh
+++ fungal_srs/hv_pipeline/Magnaporthe/dom_arch.sh
@@ -12,33 +12,33 @@ cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder
 # cp /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline/Pfam_lib/* Pfam_lib
 
 ### run pfam_scan to determine domain architecture for each OG
-cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder
-mkdir -p Pfam_Scan_out
-source activate /global/scratch/users/annen/anaconda3/envs/pfam_scan.pl
-part=${1}   # 000, 001, etc; 01 to 18
-ls OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences/OG00${part}* | while read fa; do
-    og=$(basename "${fa}")
-    if [ -f "Pfam_Scan_out/${og}.pfamscan.out" ]; then
-        echo "${og} already done"
-    else
-        echo "Running pfam_scan for ${og}"
-        pfam_scan.pl -fasta OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences/${og} -dir Pfam_lib -e_dom 0.01 -e_seq 0.01 -outfile Pfam_Scan_out/${og}.pfamscan.out
-        echo "${og} done"
-    fi
-done
-source deactivate
+# cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder
+# mkdir -p Pfam_Scan_out
+# source activate /global/scratch/users/annen/anaconda3/envs/pfam_scan.pl
+# part=${1}   # 000, 001, etc; 01 to 18
+# ls OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences/OG00${part}* | while read fa; do
+#     og=$(basename "${fa}")
+#     if [ -f "Pfam_Scan_out/${og}.pfamscan.out" ]; then
+#         echo "${og} already done"
+#     else
+#         echo "Running pfam_scan for ${og}"
+#         pfam_scan.pl -fasta OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences/${og} -dir Pfam_lib -e_dom 0.01 -e_seq 0.01 -outfile Pfam_Scan_out/${og}.pfamscan.out
+#         echo "${og} done"
+#     fi
+# done
+# source deactivate
 
 ### parse each pfam_scan output file for domain architecture of the OG
-# mkdir -p Domain_Arch
-# > Domain_Arch/OG_domarch.REPORT.txt
-# ls Pfam_Scan_out | while read ps; do
-#     og=$(echo ${ps} | awk -v FS="." '{ print $1; }')
-#     num=$(grep -c ">" OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences/${og}.fa)
-#     echo ${og}
-#     ### python script that outputs the line: OG, num_genes_in_OG, num_total_pfamscan_hits, percent_genes_with_common_arch, most_common_domarch, set_of_all_domains_in_OG_and_counts
-#     cat Pfam_Scan_out/${ps} | python /global/scratch/users/annen/KVKLab/fungal_srs/hv_pipeline/Tuning_hvPipeline/dom_arch.py ${og} ${num} >> Domain_Arch/OGs_domarch.REPORT.txt
-# done
+mkdir -p Domain_Arch
+> Domain_Arch/OG_domarch.REPORT.txt
+ls Pfam_Scan_out | while read ps; do
+    og=$(echo ${ps} | awk -v FS="." '{ print $1; }')
+    num=$(grep -c ">" OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences/${og}.fa)
+    echo ${og}
+    ### python script that outputs the line: OG, num_genes_in_OG, num_total_pfamscan_hits, percent_genes_with_common_arch, most_common_domarch, set_of_all_domains_in_OG_and_counts
+    cat Pfam_Scan_out/${ps} | python /global/scratch/users/annen/KVKLab/fungal_srs/hv_pipeline/Tuning_hvPipeline/dom_arch.py ${og} ${num} >> Domain_Arch/OGs_domarch.REPORT.txt
+done
 
 ### parse Domain_Arch/OG_domarch.REPORT.txt for 1) a list of domains and the count and 2) a list of domain archs and their count, both ordered greatest to least
-# cat Domain_Arch/OGs_domarch.REPORT.txt | python /global/scratch/users/annen/KVKLab/fungal_srs/hv_pipeline/Tuning_hvPipeline/common_dom.py > Domain_Arch/Common_Domains.REPORT.txt
-# cat Domain_Arch/OGs_domarch.REPORT.txt | python /global/scratch/users/annen/KVKLab/fungal_srs/hv_pipeline/Tuning_hvPipeline/common_arch.py > Domain_Arch/Common_Archs.REPORT.txt
+cat Domain_Arch/OGs_domarch.REPORT.txt | python /global/scratch/users/annen/KVKLab/fungal_srs/hv_pipeline/Tuning_hvPipeline/common_dom.py > Domain_Arch/Common_Domains.REPORT.txt
+cat Domain_Arch/OGs_domarch.REPORT.txt | python /global/scratch/users/annen/KVKLab/fungal_srs/hv_pipeline/Tuning_hvPipeline/common_arch.py > Domain_Arch/Common_Archs.REPORT.txt
