2f1ed14537a281a66e6df56b9b241ba72e9d95ad
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-04-19 01:11:57 -0700

    itol dom py

---
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/itol_domains.py
@@ -0,0 +1,54 @@
+from Bio import SeqIO
+import sys
+import os
+import shutil
+import csv
+
+### works in: /global/scratch/users/annen/000_FUNGAL_SRS_000/Tuning_hvPipeline
+###     OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/${OG}.fa
+###     ${OG}.Pfam.reduced.tbl
+
+og = sys.argv[1]    # OG
+fa = "OrthoFinder_out/Results_Mar16/Orthogroup_Sequences/" + og + ".fa"
+dm = og + ".Pfam.reduced.tbl"
+out = og + ".iTOL.domains.txt"
+
+record_list = list(SeqIO.parse(fa, 'fasta'))
+
+DOMAINS = {}    # key=gene_name, value=[shape,start,stop,color,domain_name]
+with open(dm, 'r') as tbl:
+    for line in tbl:
+        lst = line.split()
+        gene_name = lst[0]
+        start = lst[10]
+        stop = lst[11]
+        domain_name = lst[2]
+        if "NB-ARC" in domain_name:
+            shape = "HH"
+            color = "#ffff99"   # yellow
+        elif "LRR" in domain_name:
+            shape = "RE"
+            color = "#de4b4d"   # red
+        elif "Rx_N" in domain_name:
+            shape = "EL"
+            color = "6aeb65"   # green
+        else:
+            shape = "DI"
+            color = "#9e9e9e"   # grey
+    if DOMAINS.get(gene_name):
+        DOMAINS[gene_name].append([shape,start,stop,color,domain_name])
+    else:
+        DOMAINS[gene_name] = [[shape,start,stop,color,domain_name]]
+
+
+with open(out, 'w') as output:
+    for i in range(len(record_list)):
+        record = record_list[i]
+        gene_name = record.id
+        length = len(record.seq)
+        result = [gene_name,length]
+        for d in DOMAINS[gene_name]:
+            result.append("|".join(d))
+        #output.write(",".join(result))
+        print(",".join(result))
+
