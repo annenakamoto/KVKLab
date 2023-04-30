c2963f21ae885c691302ff09de6db0e107ee4b07
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-04-30 00:37:04 -0700

    resume raxml

--- fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
+++ fungal_srs/hv_pipeline/Tuning_hvPipeline/og_mafft_raxml.sh
@@ -25,11 +25,25 @@ OG=${1}
 raxml_dir=${2}
 aln_dir=${3}
 ### run raxml on OG alignment
-cd /global/scratch/users/annen/000_FUNGAL_SRS_000/${raxml_dir}
-echo "*********** ${OG} starting raxml ***********"
+# cd /global/scratch/users/annen/000_FUNGAL_SRS_000/${raxml_dir}
+# echo "*********** ${OG} starting raxml ***********"
+# dt=$(date '+%m/%d/%Y %H:%M:%S')
+# echo "STARTING: ${dt}"
+# raxmlHPC-PTHREADS-SSE3 -s ${aln_dir}/${OG}.afa -n RAxML.${OG} -T ${SLURM_NTASKS} -m PROTCATJTT -f a -x 12345 -p 12345 -# 100
+# dt=$(date '+%m/%d/%Y %H:%M:%S')
+# echo "DONE: ${dt}"
+
+
+### resume run for the extremely large Zm OG0000000
 dt=$(date '+%m/%d/%Y %H:%M:%S')
-echo "STARTING: ${dt}"
-raxmlHPC-PTHREADS-SSE3 -s ${aln_dir}/${OG}.afa -n RAxML.${OG} -T ${SLURM_NTASKS} -m PROTCATJTT -f a -x 12345 -p 12345 -# 100
+echo "*** starting ML best tree search: ${dt}"
+raxmlHPC-PTHREADS-SSE3 -s ${aln_dir}/${OG}.afa -n RAxML.${OG}.resume -T ${SLURM_NTASKS} -m PROTCATJTT -f d -p 12345 -# 100
+
 dt=$(date '+%m/%d/%Y %H:%M:%S')
-echo "DONE: ${dt}"
+echo "*** finished ML best tree search: ${dt}"
+echo "*** now adding bootstraps to best tree... ***"
+raxmlHPC-PTHREADS-SSE3 -s ${aln_dir}/${OG}.afa -n RAxML.${OG}.resume -T ${SLURM_NTASKS} -m PROTCATJTT -f b -t RAxML_bestTree.RAxML.${OG}.resume -z RAxML_bootstrap.RAxML.${OG} -p 12345 
+
+dt=$(date '+%m/%d/%Y %H:%M:%S')
+echo "*** DONE: ${dt}"
 
