95752561d1a9f0e1092e051d7146a78cffa3ab25
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-05-04 12:15:25 -0700

    itol domains

---
+++ fungal_srs/hv_pipeline/Mo_NBARC_NACHT_AAA/itol_domain_plot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=raxml_NBARC_NACHT_AAA
+#SBATCH --partition=savio2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=annen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/MoOrthoFinder/NBARC_NACHT_AAA_hvAnalysis
+
+DOM=${1}     ## specify which domain (NB-ARC, NACHT, AAA)
+
+module purge
+module load hmmer
+
+### generate fasta file of genes containing the domain
+module load seqtk
+cat ${DOM}.Mo.filt.afa | awk '/>/ { print $4; }' > ${DOM}.gene_list.txt
+seqtk subseq -l 60 MoPANPROTEOME_72.fa ${DOM}.gene_list.txt > ${DOM}.Mo.filt.fa
+
+source activate /global/scratch/users/annen/anaconda3/envs/R
+hmmsearch --domtblout ${DOM}.Pfam.tbl Pfam-A.hmm ${DOM}.Mo.filt.fa
+tr -s ' ' <${DOM}.Pfam.tbl > ${DOM}.Pfam.ws.tbl
+Rscript ../../../ProteinFamily/scripts/reduce_pfam.R -i ${DOM}.Pfam.ws.tbl -o ${DOM}.Pfam.reduced.tbl -e 0.01 -f 0.1 -a 10
+source deactivate
+
+source activate /global/scratch/users/annen/anaconda3/envs/Biopython
+python ../../../KVKLab/fungal_srs/hv_pipeline/Mo_NBARC_NACHT_AAA/itol_domains.py ${DOM}
+source deactivate
---
+++ fungal_srs/hv_pipeline/Mo_NBARC_NACHT_AAA/itol_domains.py
@@ -0,0 +1,81 @@
+from Bio import SeqIO
+import sys
+import os
+import shutil
+import csv
+
+### Generate domain annotations for itol
+
+dom = sys.argv[1]    # domain (NB-ARC, NACHT, AAA)
+
+fa = dom + ".Mo.filt.fa"
+dm = dom + ".Pfam.reduced.tbl"
+out = dom + ".iTOL.domains.txt"
+
+record_list = list(SeqIO.parse(fa, 'fasta'))
+
+DOMAINS = {}    # key=gene_name, value=[shape,start,stop,color,domain_name]
+with open(dm, 'r') as tbl:
+    for line in tbl:
+        lst = line.split()
+        gene_name = lst[0]
+        start = lst[10]
+        stop = lst[11]
+        domain_name = lst[2]
+        if "NB-ARC" in domain_name:
+            shape = "HH"
+            color = "#ffff99"   # yellow
+        elif "NACHT" in domain_name:
+            shape = "HH"
+            color = "#6378ff"   # darkblue
+        elif "AAA" in domain_name:
+            shape = "HH"
+            color = "#63e5ff"   # lightblue
+        elif "LRR" in domain_name:
+            shape = "RE"
+            color = "#de4b4d"   # red
+        elif "Ank" in domain_name:
+            shape = "RE"
+            color = "#ffaa00"   # yellow-orange
+        elif "WD" in domain_name:
+            shape = "RE"
+            color = "#8a3200"   # brown
+        elif "TPR" in domain_name:
+            shape = "RE"
+            color = "#ff6a00"   # orange
+        elif "Rx_N" in domain_name:
+            shape = "EL"
+            color = "#6aeb65"   # green
+        elif "Pkinase" in domain_name:
+            shape = "EL"
+            color = "#00a38b"   # teal
+        elif "PNP_UDP" in domain_name:
+            shape = "EL"
+            color = "#b163ff"   # purple
+        else:
+            shape = "DI"
+            color = "#9e9e9e"   # grey
+        if DOMAINS.get(gene_name):
+            item = [str(shape),str(start),str(stop),str(color),str(domain_name)]
+            DOMAINS[str(gene_name)].append(item)
+        else:
+            item = [str(shape),str(start),str(stop),str(color),str(domain_name)]
+            DOMAINS[str(gene_name)] = [item]
+
+with open(out, 'w') as output:
+    output.write("DATASET_DOMAINS\n")
+    output.write("SEPARATOR COMMA\n")
+    output.write("DATASET_LABEL,Domains\n")
+    output.write("COLOR,#ff0000\n")
+    output.write("DATA\n")
+    for i in range(len(record_list)):
+        record = record_list[i]
+        gene_name = record.id
+        length = len(record.seq)
+        result = [str(gene_name),str(length)]
+        if DOMAINS.get(gene_name):
+            for d in DOMAINS[gene_name]:
+                result.append("|".join(d))
+            output.write(",".join(result) + "\n")
+            #print(",".join(result))
+
