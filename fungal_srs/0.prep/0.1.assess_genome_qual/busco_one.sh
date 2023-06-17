ca35c66806ac9d2c3171537358f61646dc9b72ad
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-17 10:12:20 -0700

    busco zymo

--- fungal_srs/0.prep/0.1.assess_genome_qual/busco_one.sh
+++ fungal_srs/0.prep/0.1.assess_genome_qual/busco_one.sh
@@ -10,11 +10,16 @@
 working_dir=${1}    # full path to working directory
 genome_path=${2}    # path to genome from working directory, not including fna file name
 genome_name=${3}    # isolate name or other identifier 
+lineage=${4}        # the busco dataset lineage
+    ### Mo: sordariomycetes_odb10
+    ### Zt: dothideomycetes_odb10
+    ### Sc: saccharomycetes_odb10
+    ### Nc: sordariomycetes_odb10
 
 cd ${working_dir}
 
 ### run busco on a genome
 module purge    # loaded modules interfere with busco
 source activate /global/scratch/users/annen/anaconda3/envs/busco
-busco -i ${genome_path}/${genome_name}.fna -o ${genome_name} -l sordariomycetes_odb10 -m genome -c ${SLURM_NTASKS} -f
+busco -i ${genome_path}/${genome_name}.fna -o ${genome_name} -l ${lineage} -m genome -c ${SLURM_NTASKS} -f
 conda deactivate
