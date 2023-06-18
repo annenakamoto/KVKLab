c85e0def1c29a4ed9ba60d923d9f449b91f96955
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-18 14:56:53 -0700

    busco tree

--- fungal_srs/0.prep/0.2.busco_genome_tree/1.busco_phylo.sh
+++ fungal_srs/0.prep/0.2.busco_genome_tree/1.busco_phylo.sh
@@ -17,10 +17,10 @@ species=${2}        ## Species shorthand, ie. Mo, Zt, Sc, Nc
 cd ${working_dir}
 
 ### run busco phylogenomics to generate supermatrix concatenated alignment
-# module purge
-# source activate /global/scratch/users/annen/anaconda3/envs/BUSCO_phylogenomics
-# python /global/scratch/users/annen/BUSCO_phylogenomics/BUSCO_phylogenomics.py -i BUSCO -o ${species}BUSCO_PHYLO -t ${SLURM_NTASKS} --supermatrix_only --gene_tree_program fasttree > BUSCO_phylogenomics.LOG.txt
-# conda deactivate
+module purge
+source activate /global/scratch/users/annen/anaconda3/envs/BUSCO_phylogenomics
+python /global/scratch/users/annen/BUSCO_phylogenomics/BUSCO_phylogenomics.py -i BUSCO -o ${species}BUSCO_PHYLO -t ${SLURM_NTASKS} --supermatrix_only --gene_tree_program fasttree > BUSCO_phylogenomics.LOG.txt
+conda deactivate
 
 module load fasttreeMP
 FastTreeMP -gamma -out ${species}.FASTTREE ${species}BUSCO_PHYLO/supermatrix/SUPERMATRIX.fasta
