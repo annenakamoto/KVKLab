e8d25c81aa7af38147d5ef7c4a943a905901c399
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-17 22:09:45 -0700

    assemble zymo

--- fungal_srs/0.prep/0.2.busco_genome_tree/assm_zymo.sh
+++ fungal_srs/0.prep/0.2.busco_genome_tree/assm_zymo.sh
@@ -10,21 +10,31 @@
 ### assemble the Zymo genome specified
 ### follows: https://github.com/afeurtey/WW_PopGen/blob/master/Data_prep/Assemble_array.sh
 
+cd /global/scratch/users/annen/000_FUNGAL_SRS_000/ZYMO/1000_assemble
+
 sample=${1} # to name assembly
 read_prefix=${2} # SRA accession
 
-cd /global/scratch/users/annen/000_FUNGAL_SRS_000/ZYMO/1000_assemble
+### get the fastq files from SRA
 module purge
+source activate /global/scratch/users/annen/anaconda3/envs/sra-tools
+while [ ! -d "${read_prefix}" ]; do
+    prefetch -p ${read_prefix}
+done
+fasterq-dump ${read_prefix}
+conda deactivate
+
 module load spades
 
 read1=${read_prefix}_1.fastq
 read2=${read_prefix}_2.fastq
 
 echo ${sample} ${read1} ${read2} ;
+mkdir -p ${sample}
 
 spades.py  \
-  -o ${sample}.fna \
   --careful \
   --threads ${SLURM_NTASKS} \
   -1 ${read1} \
-  -2 ${read2}
+  -2 ${read2} \
+  -o ${sample}
