01679a52551957a940b77c2e81b78945dcb3131b
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-26 16:41:42 -0700

    pre orthofinder

--- fungal_srs/0.prep/0.3.gene_annotation/pre_orthofinder.sh
+++ fungal_srs/0.prep/0.3.gene_annotation/pre_orthofinder.sh
@@ -20,5 +20,5 @@ while read genome; do
     ### rename faa file and genes in file, remove stop codons
     cat ${genome}/fungap_out/fungap_out/fungap_out_prot.faa | awk -v g=${genome} '/>/ { print ">" g substr($1,6,6); } !/>/ { print; }' | tr -d "*" > ../ORTHOFINDER/FAA/${genome}.faa
     ### rename gff file and genes in gff file (replace any "gene_" with the new_name)
-    sed "s/gene\_/${new_name}\_/g" ${genome}/fungap_out/fungap_out/fungap_out.gff3 > ../ORTHOFINDER/GFF/${genome}.gff3
+    sed "s/gene\_/${genome}\_/g" ${genome}/fungap_out/fungap_out/fungap_out.gff3 > ../ORTHOFINDER/GFF/${genome}.gff3
 done < ../fungap_list.txt
