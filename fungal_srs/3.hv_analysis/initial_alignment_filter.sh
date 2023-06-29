2c4682f40f1bde3a0ad5718bcd1a8073d5fe008f
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-29 09:33:42 -0700

    filter OG alignments

--- fungal_srs/3.hv_analysis/initial_alignment_filter.sh
+++ fungal_srs/3.hv_analysis/initial_alignment_filter.sh
@@ -11,10 +11,11 @@
 
 working_dir=${1}    ## dir containing OG alignments
 mgf=${2}            ## 0.9 standard, 1 to only mask if all rows have the gap
+out_file=${3}       ## output file name, ex. Mo_OGalign_filt_GAP.txt, Mo_OGalign_filt_NOGAP.txt
 
 cd /global/scratch/users/annen
 
 module purge
 source activate /global/scratch/users/annen/anaconda3/envs/R
-Rscript KVKLab/fungal_srs/3.hv_analysis/assess_aln_cutoff_dist.R -d ${working_dir} -f ${mgf} -w 3
+Rscript KVKLab/fungal_srs/3.hv_analysis/assess_aln_cutoff_dist.R -d ${working_dir} -f ${mgf} -w 3 | awk '/OG0/ { print substr($2,2,9) "\t" $3 "\t" substr($4,1,length($4)-1); }' > ${working_dir}/../${out_file}
 conda deactivate
