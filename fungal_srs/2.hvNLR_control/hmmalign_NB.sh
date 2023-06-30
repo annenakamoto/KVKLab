cf720b42296831d3c60a545f990623c46b517158
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-30 15:00:31 -0700

    align OGs in range

--- fungal_srs/2.hvNLR_control/hmmalign_NB.sh
+++ fungal_srs/2.hvNLR_control/hmmalign_NB.sh
@@ -28,7 +28,7 @@ hmmsearch -A ${species}.${DOM}.sto --cpu ${SLURM_NTASKS} ${DOM}.hmm ${species}_P
 esl-reformat --mingap -o ${species}.${DOM}.nogap.sto afa ${species}.${DOM}.sto                 ## remove all-gap columns
 leng=$(grep LENG ${DOM}.hmm | awk '{ print int($2*0.7) }')
 echo "${DOM} hmm 70% length: ${leng}"
-esl-alimanip -o ${species}.${DOM}.filt.sto --lmin 30 ${species}.${DOM}.nogap.sto          ## remove sequences with less than 30 AA
+esl-alimanip -o ${species}.${DOM}.filt.sto --lmin ${leng} ${species}.${DOM}.nogap.sto          ## remove sequences with less than 30 AA
 esl-reformat -o ${species}.${DOM}.filt.afa afa ${species}.${DOM}.filt.sto                      ## reformat to fasta
 hmmbuild ${species}.${DOM}.hmm ${species}.${DOM}.filt.afa                                      ## rebuild profile to be species-specific
 
