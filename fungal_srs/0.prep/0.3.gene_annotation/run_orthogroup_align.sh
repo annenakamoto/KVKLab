fc094f8ea910c042240a5a74a2c2adaec1581f1f
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-06-30 10:59:56 -0700

    align OGs in range

--- fungal_srs/0.prep/0.3.gene_annotation/run_orthogroup_align.sh
+++ fungal_srs/0.prep/0.3.gene_annotation/run_orthogroup_align.sh
@@ -13,13 +13,18 @@ working_dir=${1}    ## ORTHOFINDER directory for the species
 
 cd ${working_dir}
 mkdir -p OG_ALIGNMENTS
+prefix=OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences
+
+start=1             ## align all OGs if args 2 and 3 not specified
+stop=$(ls ${prefix} | wc -l)
+start=${2}          ## align OGs in index range start to stop, inclusive, if specified
+stop=${3}
 
 module purge
 source activate /global/scratch/users/annen/anaconda3/envs/mafft
 
 ### align with mafft
-prefix=OrthoFinder_out/Results_out/WorkingDirectory/OrthoFinder/Results_out/Orthogroup_Sequences
-ls ${prefix} | while read fa; do
+ls ${prefix} | sed -n "${start},${stop}p" | while read fa; do
     OG=$(echo ${fa} | awk '{ print substr($1,1,length($1)-3); }')
     echo "aligning ${OG}..."
     mafft --maxiterate 1000 --localpair --thread ${SLURM_NTASKS} --quiet ${prefix}/${OG}.fa > OG_ALIGNMENTS/${OG}.afa
