ad26a2909858eb7b847417aff037d37498e91c7a
Anne Nakamoto <annenakamoto@Annes-MacBook-Pro.local>
2023-08-01 17:42:14 -0700

    nlr clade refinement

--- fungal_srs/2.hvNLR_control/refinement_subclades.sh
+++ fungal_srs/2.hvNLR_control/refinement_subclades.sh
@@ -49,7 +49,7 @@ echo -e "GENE\tREFINED_CLADE\tFINAL_CLADE\tHV\tHVSITES\tHVSITES_LEN_NORM" > ${ou
 cat ${out_dir}.HVSITE_RESULTS.txt | while read line; do
     clade_f=$(echo ${line} | awk '{ print $1; }')
     clade_i=$(echo ${clade_f} | awk '{ split($1,n,"_"); print n[1] "_" n[2] "_" n[3]; }')
-    cat ${out_dir}/${clade_i}.subclade_list.txt | awk -v data=${line} -v cf=${clade_f} '$3 ~ cf { print $1 "\t" substr(data,1,length(data)-1); }' >> ${out_dir}.GENE_TABLE.txt
+    cat ${out_dir}/${clade_i}.subclade_list.txt | awk -v data="${line}" -v cf=${clade_f} '$3 ~ cf { print $1 "\t" substr(data,1,length(data)-1); }' >> ${out_dir}.GENE_TABLE.txt
     hv=$(echo ${line} | awk '{ print $3; }')
     if [ "${hv}" -eq "1" ]; then
         cat ${out_dir}/${clade_i}.subclade_list.txt | awk -v cf=${clade_f} '$3 ~ cf { print $1 ",label,node,#000000,1,normal,#fff93d"; }' >> ${out_dir}.HV_HILIGHT.iTOL.txt
